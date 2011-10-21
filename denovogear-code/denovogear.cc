68318cb8e0cb47534965507449e39878301bbec1
Avinash Ramu <trackavinash@gmail.com>
2011-10-21 12:05:29 -0500

    variable mu based on indel lengths, internal lookup table generation, modified lookup table,  cmd line parameters
    Former-commit-id: de99d7c5383ea61f6d8c86d80c20176124bb068f [formerly 4a053ca1357f5d9ab2c4560a20391cdb1d668e07 [formerly dd0c9b282da798c1fcee1ddbbf49100527915fe6]]
    Former-commit-id: 4a053ca1357f5d9ab2c4560a20391cdb1d668e07
    Former-commit-id: 869a786cd5b71e3d4325b43fa66732ec4f97258a

--- denovogear-code/denovogear.cc
+++ denovogear-code/denovogear.cc
@@ -3,6 +3,7 @@
 #include <assert.h>
 #include <stdlib.h>
 #include <math.h>
+#include <getopt.h>
 #include "bcf.h"
 #include "bgzf.h"
 #include "denovogear.h"
@@ -33,71 +34,119 @@ using namespace RBD_LIBRARIES;
 #define MIN_MAPQ 40 
 
 
-int parse(char* ped_file, char* bcf_file) 
+int parse(char* ped_file, char* bcf_file, double snp_mrate, double indel_mrate,
+		  double poly_rate, double mu_scale) 
 {
-   	printf("\nPED file : %s, BCF file : %s", ped_file, bcf_file);   
-   	int trio_count = parse_ped(ped_file, trios); // Parse PED files and read in trios
-   	printf ("\nThe number of trios in the ped file : %d\n", trio_count); 
-   
+	printf("\nPED file : %s, BCF file : %s", ped_file, bcf_file);   
+	int trio_count = parse_ped(ped_file, trios); // Parse PED files and read in trios
+	printf ("\nThe number of trios in the ped file : %d\n\n", trio_count);
 	
-   
-   	/*
-   	for(i=0; i<trio_count; i++) {
-		printf("\n%s %s %s %s\n", trios[i].fID, trios[i].cID, trios[i].mID, trios[i].dID);
-   	}
-   	*/
-     	
 	vector<vector<string > > tgt;
+	vector<string> tmpV;
+	for (int l=0;l<10;l++)
+		tmpV.push_back("NA");
+	for (int l=0;l<100;l++)
+		tgt.push_back(tmpV);
 	lookup_t lookup;
-    read_lookup(tgt, lookup);
-    
+	makeLookup("point", snp_mrate, indel_mrate, poly_rate, tgt, lookup);	
+	cout<<"\nRead SNP lookup table\n";
+	cerr <<" First mrate "<< lookup.mrate(1,1) <<" Last "<< lookup.mrate(100,10)<<endl;
+	cerr <<" First code "<< lookup.code(1,1) <<" Last "<< lookup.code(100,10)<<endl;
+	cerr <<" First tgt "<< tgt[0][0] <<" Last "<< tgt[99][9] <<endl;
+	cerr <<" First tref "<< lookup.tref(1,1) <<" Last "<< lookup.tref(100,10) <<endl;
+
 	vector<vector<string > > tgtIndel;
-	lookup_t lookupIndel;   	
-    read_indelLookup(tgtIndel, lookupIndel);
-    qcall_t mom_snp, dad_snp, child_snp;
-   	indel_t mom_indel, dad_indel, child_indel;
-   	
-   	
-   	
-   	//printf("\nfID 0 %s", trios[0].fID);
+	vector<string> tmpIndel;
+    for (int l=0;l<3;l++)
+        tmpIndel.push_back("NA");
+    for (int l=0;l<9;l++)
+        tgtIndel.push_back(tmpIndel);
+	lookup_t lookupIndel;
+	makeLookup("indel", snp_mrate, indel_mrate, poly_rate, tgtIndel, lookupIndel);
+	cout<<"\nRead indel lookup table";
+	cerr <<endl<<" First mrate "<< lookupIndel.mrate(1,1) <<" Last "<< lookupIndel.mrate(9,3)<<endl;
+    cerr <<" First code "<< lookupIndel.code(1,1) <<" Last "<< lookupIndel.code(9,3)<<endl;
+    cerr <<" First tgt "<< tgtIndel[0][0] <<" Last "<< tgtIndel[8][2] <<endl;
+    cerr <<" First prior "<< lookupIndel.priors(1,1) <<" Last "<< lookupIndel.priors(9,3) <<endl;
+
+	qcall_t mom_snp, dad_snp, child_snp;
+	indel_t mom_indel, dad_indel, child_indel;
+	
 	bcf_hdr_t *hout, *hin;
 	bcf_t *bp = vcf_open(bcf_file, "rb");
 	hin = hout = vcf_hdr_read(bp);
 	bcf1_t *b;
 	b = static_cast<bcf1_t *> (calloc(1, sizeof(bcf1_t)));  
 	while ( vcf_read(bp, hin, b) > 0 ) {
-   		int j = 0, flag =0;
-   		//printf("Position Number %d", pos++);
-   		for ( j=0; j<trio_count; j++) {
-   			int is_indel = bcf_2qcall(hout, b, trios[j],  &mom_snp, &dad_snp, &child_snp, &mom_indel, &dad_indel, &child_indel, flag);
-   			//cout<<"\nis_indel is "<<is_indel;
-   			
-   			if ( is_indel == 0 ) {
-   				trio_like_snp(child_snp, mom_snp, dad_snp, flag, tgt, lookup);   			
-   			}   
-   			else if ( is_indel == 1 ) {
-			    trio_like_indel(&child_indel, &mom_indel, &dad_indel, flag, tgtIndel, lookupIndel);  
-   			}
-   			else if ( is_indel < 0 ) {
-   				printf("\n BCF PARSING ERROR !  %d", is_indel);
-   				printf("\n Exiting !");
-   				exit(1);
-   			}
-   		}   		  
+		int j = 0, flag =0;
+		//printf("Position Number %d", pos++);
+		for ( j=0; j<trio_count; j++) {
+			int is_indel = bcf_2qcall(hout, b, trios[j],  &mom_snp, &dad_snp, 
+									  &child_snp, &mom_indel, &dad_indel, 
+									  &child_indel, flag);
+			if ( is_indel == 0 ) {
+				trio_like_snp(child_snp, mom_snp, dad_snp, flag, tgt, lookup);   			
+			}   
+			else if ( is_indel == 1 ) {
+				trio_like_indel(&child_indel, &mom_indel, &dad_indel, flag, 
+								tgtIndel, lookupIndel, mu_scale);  
+			}
+			else if ( is_indel < 0 ) {
+				printf("\n BCF PARSING ERROR !  %d", is_indel);
+				printf("\n Exiting !");
+				exit(1);
+			}
+		}   		  
 	}
-	
+
 	bcf_hdr_destroy(hin);
 	bcf_destroy(b);
-    bcf_close(bp);
-   	return 0;
+	bcf_close(bp);
+	return 0;
    
 }
 
 
 int main(int argc, char *argv[])
 {
+	char ped_f[50] = "EMPTY", bcf_f[50] = "EMPTY";
+	double indel_mrate = 1e-9, snp_mrate = 1e-8, poly_rate = 1e-3, mu_scale = 1.0;
 
-  parse(argv[1], argv[2]);
-  printf("\nDone !\n");
-  return 0;
+	while (1) {
+		int option_index = 0;
+		static struct option long_options[] = {{"ped", 1, 0, 0}, 
+			{"bcf", 1, 0, 1}, {"snp_mrate", 1, 0, 2}, {"indel_mrate", 1, 0, 3}, 
+			{"poly_rate", 1, 0, 4}, {"indel_mu_scale", 1, 0, 5}};
+		int c = getopt_long (argc, argv, "", long_options, &option_index);
+		if (c == -1)
+			break;
+		switch(c) 
+		{      
+			case 0:
+				strcpy(ped_f, optarg);
+				break;
+			case 1:
+				strcpy(bcf_f, optarg);
+				break;
+			case 2:
+				snp_mrate = atof(optarg);
+				break;
+			case 3:
+				indel_mrate = atof(optarg);
+				break;
+			case 4:
+				poly_rate = atof(optarg);
+				break;
+			case 5:
+				mu_scale = atof(optarg);
+				break;
+		}
+	}
+	if(!strcmp(bcf_f, "EMPTY") || !strcmp(ped_f, "EMPTY")) {
+		cout<<"ERROR ! Please specify both the PED file and BCF file ! Exiting!\n";
+		exit(1);
+	}
+	parse(ped_f, bcf_f, snp_mrate, indel_mrate, poly_rate, mu_scale);
+	cerr<<"\nDone !\n";
+	return 0;
 }
--- denovogear-code/indelLike.cc
+++ denovogear-code/indelLike.cc
@@ -1,135 +1,16 @@
-#include <stdio.h>
-#include <string.h>
-#include <assert.h>
-#include <stdlib.h>
-#include <math.h>
-
 #include <vector>
 #include <iostream>
-#include <fstream>
-#include <sstream>
-
-#define WANT_STREAM       // include iostream and iomanipulators
-
-#include "bcf.h"
 #include "parser.h"
 #include "lookup.h"
-
-
-#include "bgzf.h"
 #include "newmatap.h"
 #include "newmatio.h"
 
-#ifdef use_namespace
-using namespace RBD_LIBRARIES;
-#endif
-
-#ifndef VERSION
-#define VERSION "dummy"
-#endif
-
-#define MRATE 5e-7
-#define MIN_READ_DEPTH 0
-#define MIN_READ_DEPTH_INDEL 10
-#define MIN_MAPQ 40 
-
-int read_indelLookup (vector<vector<string> > &tgtIndel, lookup_t & lookupIndel);
-void trio_like_indel(indel_t *child, indel_t *mom, indel_t *dad, int flag, vector<vector<string> > &tgtIndel, lookup_t & lookupIndel);
-
-
 using namespace std;
 
-/* commands */
-int read_indelLookup( vector<vector<string > >  & tgtIndel, lookup_t & lookupIndel)
-{
-            string line;
-            int tmp[27];
-            float tmp2[27];
-            float tmp3[27];
-            float tmp4[27];
-            float tmp5[27];
-            float tmp6[27];
-            float tmp7[27];
-
-
-  vector<string> tmpIndel;
-    lookupIndel.priors.resize(9, 3);
-    lookupIndel.snpcode.resize(9,3);  
-    lookupIndel.code.resize(9,3);  
-    lookupIndel.tp.resize(9,3);  
-    lookupIndel.mrate.resize(9,3);
-    lookupIndel.denovo.resize(9,3);
-    lookupIndel.norm.resize(9,3);
-  
-  for (int l=0;l<3;l++)
-     tmpIndel.push_back("NA");
-  for (int l=0;l<9;l++)
-     tgtIndel.push_back(tmpIndel);
-
-                //vector<string> tmp4; 
-            float blah;
-            int i=0,j=0,k=0;
-            string blah2;
-            
-            ifstream lookup_file("lookup_indel.txt");
-            if (!lookup_file) {
-                cerr <<"cannot open lookup table"<<endl; 
-                exit(1);
-            }
-            
-            while (getline(lookup_file,line)) {
-                istringstream iss(line);
-                iss >> blah;
-                tmp[k]= (int) blah;
-                iss >> blah;
-                tmp2[k]=blah;
-                iss >> blah;
-                tmp3[k]=blah;
-                iss >> blah2;
-                tgtIndel[i][j++]=blah2;
-                
-                if (j==3) {
-                    i++;
-                    j=0;
-                }
-                
-                iss >> blah;
-                tmp4[k]=blah;
-                iss >> blah;
-                tmp5[k]=blah;
-                iss >> blah;
-                tmp6[k]=blah;
-                iss >> blah;
-                tmp7[k]=blah;
-            
-                k++;
-            }
-            lookup_file.close();
-            lookupIndel.code << tmp2;
-            lookupIndel.tp << tmp3;
-            lookupIndel.snpcode << tmp;
-            lookupIndel.mrate << tmp4;
-            lookupIndel.denovo << tmp5;
-            lookupIndel.norm << tmp6;
-            lookupIndel.priors << tmp7;
-            
-
-            cerr<<"\n Read "<<k <<" elements from INDEL lookup"<<endl;
-            cerr <<" First mrate"<< lookupIndel.mrate(1,1) <<" Last "<< lookupIndel.mrate(9,3)<<endl;
-            cerr <<" First "<< lookupIndel.code(1,1) <<" Last "<< lookupIndel.code(9,3)<<endl;
-            cerr <<" First "<< tgtIndel[0][0] <<" Last "<< tgtIndel[8][2] <<endl;
-            cerr <<" First "<< lookupIndel.priors(1,1) <<" Last "<< lookupIndel.priors(9,3) <<endl;
-            return 0; 
-        }
-
-
-
-void trio_like_indel(indel_t *child,indel_t *mom, indel_t *dad, int flag, vector<vector<string > > & tgtIndel, lookup_t & lookupIndel)
-{
-    
-    
-    
-    
+void trio_like_indel(indel_t *child,indel_t *mom, indel_t *dad, int flag, 
+					 vector<vector<string > > & tgtIndel, 
+					 lookup_t & lookupIndel, double mu_scale)
+{    
     Real a[3];   
     Real maxlike_null,maxlike_denovo,pp_null,pp_denovo,denom,numer;       
     Matrix M(1,3);
@@ -142,63 +23,72 @@ void trio_like_indel(indel_t *child,indel_t *mom, indel_t *dad, int flag, vector
     Matrix DN(9,3);
     Matrix PP(9,3);
     int i,j,k,l;
-
-
-
     int coor = child->pos;
     char ref_name[3];
     strcpy( ref_name, child->chr); // Name of the reference sequence
-    
-
-    
-    // this is the complex case where child has more than 1 alt, ignore for now
-    if (strstr(child->alt, ",")!=0) {
-        // printf("\nMultiple alt, position %ld\n", child->pos); // testing - Avinash 
+        
+    // this is the case where child has more than 1 alt, ignore for now
+    if (strstr(child->alt, ",") != 0) {
     	return;
     }
 
-
-    //Currently, samtools creates indel records are created for every trio in the BCF regardless of whether they have data
+	bool is_insertion;
+	int len_diff = strlen(mom->ref_base) - strlen(mom->alt);
+	if (len_diff < 0) {
+		is_insertion = true;
+		len_diff = -len_diff;
+	} else if (len_diff > 0) {
+		is_insertion = false;
+	}
+	//cout<<endl<<mom->ref_base<<" "<<mom->alt<<" "<<len_diff<<" "<<is_insertion;
+	double indel_mrate, new_indel_mrate;
+	if(is_insertion) 
+		indel_mrate = mu_scale * (-22.8689 - (0.2994 * len_diff));
+	else
+		indel_mrate = mu_scale * (-21.9313 - (0.2856 * len_diff));	
+	indel_mrate = exp(indel_mrate);
+	
+	for (int j = 1; j <= 9; j++)
+		for (int l = 1; l <= 3; l++) {
+			new_indel_mrate = pow(indel_mrate, lookupIndel.hit(j,l));
+			lookupIndel.mrate(j,l) = new_indel_mrate;
+		}
+	
+	//Currently, samtools creates indel records are created for every trio in the BCF regardless of whether they have data
     //skip sites with both parents missing or child data missing (could still see de novo with data in child and one parent e.g. DD/NN/RR)
-    if (child->depth < MIN_READ_DEPTH_INDEL){return;}
-    if (mom->depth < MIN_READ_DEPTH_INDEL && dad->depth < MIN_READ_DEPTH_INDEL ) {return;}    
-
-
+    if (child->depth < MIN_READ_DEPTH_INDEL ||
+		(mom->depth < MIN_READ_DEPTH_INDEL && dad->depth < MIN_READ_DEPTH_INDEL)) {
+		return;
+	}
     
-    //Load vectors    
+    //Load likelihood vectors    
     for (j = 0; j != 3; ++j) a[j]=pow(10,-mom->lk[j]/10.);
     M<<a;
     for (j = 0; j != 3; ++j) a[j]=pow(10,-dad->lk[j]/10.);
     D<<a;
     for (j = 0; j != 3; ++j) a[j]=pow(10,-child->lk[j]/10.);
     C<<a;
-    
-    
-	P=KP(M,D);    
-	F=KP(P,C);    
-	T=SP(F,lookupIndel.tp); //combine with transmission probs 
-       
-    L  = SP(T, lookupIndel.priors);
+      
+	P = KP(M,D);    
+	F = KP(P,C);    
+	T = SP(F,lookupIndel.tp); //combine with transmission probs 
+    L = SP(T, lookupIndel.priors);
 	DN = SP(L,lookupIndel.mrate);    
     PP = SP(DN,lookupIndel.norm);   //zeroes out configurations with mendelian error
     maxlike_null = PP.maximum2(i,j);       
     
 	//Find max likelihood of de novo trio configuration
-	PP=SP(DN,lookupIndel.denovo);   //zeroes out configurations with mendelian inheritance
-    maxlike_denovo=PP.maximum2(k,l); 
-    
-    
-    
+	PP = SP(DN,lookupIndel.denovo);   //zeroes out configurations with mendelian inheritance
+    maxlike_denovo = PP.maximum2(k,l); 
+       
     //make proper posterior probs
-    
-    denom=DN.sum();
-    numer=PP.sum();
-    pp_denovo=maxlike_denovo/denom;
-    pp_null=1-pp_denovo;
-    
-    
-    if ( pp_denovo > 0.001 ) {
-    	cout<<"\nDENOVO-INDEL CHILD ID: "<<child->id;
+    denom = DN.sum();
+    numer = PP.sum();
+    pp_denovo = maxlike_denovo/denom;
+    pp_null = 1 - pp_denovo;
+       
+    if ( pp_denovo > 0.0001 ) {
+    	cout<<"\nDENOVO-INDEL child id: "<<child->id;
     	cout<<" ref_name: "<<ref_name<<" coor: "<<coor<<" ref_base: "<<mom->ref_base<<" ALT: "<<mom->alt;
     	cout<<" maxlike_null: "<<maxlike_null<<" pp_null: "<<pp_null<<" tgt: "<<tgtIndel[i-1][j-1];
     	cout<<" snpcode: "<<lookupIndel.snpcode(i,j)<<" code: "<<lookupIndel.code(i,j);
