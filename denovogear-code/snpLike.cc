4f323c5a2a920a4f389d6a787e61c6817408ae62
Avinash Ramu <trackavinash@gmail.com>
2011-10-27 15:37:38 -0500

    cleanup
    Former-commit-id: 617150727538b266304eaf078d9b627ede5777af [formerly 8b711367391e2f518b4c4d9483eee2ff22aa3687 [formerly cb185918e5d1b229ca20182976006bda31cd882d]]
    Former-commit-id: 4084dba2e73a837483d0d34b6c9c3fdb30419506 [formerly 6b272374291e7880048856b81c367d93540ba419]
    Former-commit-id: cf3fb4d6c564a10cef89485901aff8e661f43e15

--- denovogear-code/snpLike.cc
+++ denovogear-code/snpLike.cc
@@ -5,12 +5,15 @@
 #include "newmatap.h"
 #include "newmatio.h"
 
+#define MIN_READ_DEPTH_SNP 10
+
 using namespace std;
 
+// Calculate DNM and Null PP
 void trio_like_snp(qcall_t child, qcall_t mom, qcall_t dad, int flag, vector<vector<string > > & tgt, lookup_t & lookup)
 {
   Real a[10];   
-  Real maxlike_null,maxlike_denovo,pp_null,pp_denovo,denom,numer;   
+  Real maxlike_null,maxlike_denovo,pp_null,pp_denovo,denom;   
   Matrix M(1,10);
   Matrix C(10,1);
   Matrix D(10,1);
@@ -25,12 +28,13 @@ void trio_like_snp(qcall_t child, qcall_t mom, qcall_t dad, int flag, vector<vec
   char ref_name[3];
   strcpy( ref_name, child.chr); // Name of the reference sequence
   
+  // Filter low read depths
   if (child.depth < MIN_READ_DEPTH_SNP ||
       (mom.depth < MIN_READ_DEPTH_SNP && dad.depth < MIN_READ_DEPTH_SNP)) {
     return;
   }
   
-  //Load vectors 
+  //Load Likelihood matrices L(D|Gm), L(D|Gd) and L(D|Gc)
   for (j = 0; j != 10; ++j)	{ 
   	//cerr<<"\n position "<<coor<<" mom lik "<<mom.lk[j]<<" dad lik "<<dad.lk[j]<<" child lik "<<child.lk[j]; 
   	a[j]=pow(10,-mom.lk[j]/10.); 
@@ -47,8 +51,10 @@ void trio_like_snp(qcall_t child, qcall_t mom, qcall_t dad, int flag, vector<vec
         
   P=KP(M,D);
   F=KP(P,C);
-  T=SP(F,lookup.tp); //combine with transmission probs 
+  // Combine transmission probs L(Gc | Gm, Gf)
+  T=SP(F,lookup.tp); 
 
+  // Combine prior L(Gm, Gf) 
   switch(mom.ref_base) {
     case 'A':
 	  L=SP(T,lookup.aref); break;
@@ -66,20 +72,22 @@ void trio_like_snp(qcall_t child, qcall_t mom, qcall_t dad, int flag, vector<vec
   }
 
   DN=SP(L,lookup.mrate);
+
+  // Find max likelihood of null configuration
   PP=SP(DN,lookup.norm);   //zeroes out configurations with mendelian error
   maxlike_null = PP.maximum2(i,j);   
   
-  //Find max likelihood of de novo trio configuration
+  // Find max likelihood of de novo trio configuration
   PP=SP(DN,lookup.denovo);   //zeroes out configurations with mendelian inheritance
   maxlike_denovo=PP.maximum2(k,l); 
 
-  //make proper posterior probs
   denom=DN.sum();
-  numer=PP.sum();
-  pp_denovo=maxlike_denovo/denom;
-  pp_null=1-pp_denovo;
 
-  if ( pp_denovo > 0.001 ) {
+  pp_denovo=maxlike_denovo/denom; // denovo posterior probability
+  pp_null=1-pp_denovo; // null posterior probability
+
+  // Check for PP cutoff 
+  if ( pp_denovo > 0.1 ) {
     cout<<"\nDENOVO-SNP CHILD ID: "<<child.id;
     cout<<" ref_name: "<<ref_name<<" coor: "<<coor<<" ref_base: "<<mom.ref_base<<" ALT: "<<mom.alt;
     cout<<" maxlike_null: "<<maxlike_null<<" pp_null: "<<pp_null<<" tgt: "<<tgt[i-1][j-1];
