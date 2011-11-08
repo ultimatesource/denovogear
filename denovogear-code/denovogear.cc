f7bf10b0385b2efd0df9cf6b53b0fb647448cb61
Avinash Ramu <trackavinash@gmail.com>
2011-11-08 13:12:49 -0600

    paired sample DNG detection
    Former-commit-id: f4f4249a8db8b5863f34fd5f9d23b7f3e2ba34a6 [formerly 8f3b03ddf45ebf92e9d1967898451e3279450573] [formerly bb90b9966b1a66d56a421c4004d199c5717c0093 [formerly 0a4a94176f7d305848a4533fa8b7fb6849a40efd]]
    
    Former-commit-id: 79ebaa099007db13d657ee46bc57865294003d1e [formerly 7be59a3eb9d61d8baf9950664bca78ecafd448a3]
    Former-commit-id: 96c270778375c86938bb33442fedc17cba2cf12a

--- denovogear-code/denovogear.cc
+++ denovogear-code/denovogear.cc
@@ -7,7 +7,6 @@
 
 
 #include "bcf.h"
-
 #include "bgzf.h"
 #include "denovogear.h"
 
@@ -37,24 +36,29 @@ using namespace RBD_LIBRARIES;
 #define MIN_MAPQ 40 
 
 
-int parse(char* ped_file, char* bcf_file, double snp_mrate, double indel_mrate,
-		  double poly_rate, double mu_scale) 
+int findDenovo(char* ped_file, char* bcf_file, double snp_mrate, 
+               double indel_mrate, double poly_rate, double pair_mrate, double mu_scale) 
 {
-	printf("\nPED file : %s, BCF file : %s", ped_file, bcf_file);   
+	printf("\nPED file : %s, BCF file : %s", ped_file, bcf_file); 
+  
 	// Parse PED files and read in trios
     Trio trios[ MAX_TRIOS ];
-    int trio_count = parse_ped(ped_file, trios); 
-	printf ("\nThe number of trios in the ped file : %d\n\n", trio_count);
+    Pair pairs[ MAX_PAIRS ];
+    int trio_count = 0, pair_count = 0;
+    parse_ped (ped_file, trios, pairs, trio_count, pair_count); 
+	printf ("\nThe number of trios in the ped file : %d", trio_count);
+    printf ("\nThe number of paired samples in the ped file : %d\n\n", pair_count);
+
 	
     // Create SNP lookup
 	vector<vector<string > > tgt;
 	vector<string> tmpV;
-	for (int l=0;l<10;l++)
+	for (int l=0; l<10; l++)
 		tmpV.push_back("NA");
-	for (int l=0;l<100;l++)
+	for (int l=0; l<100; l++)
 		tgt.push_back(tmpV);
-	lookup_t lookup;
-	makeLookup("point", snp_mrate, indel_mrate, poly_rate, tgt, lookup);	
+	lookup_snp_t lookup;
+	makeSNPLookup(snp_mrate, poly_rate, tgt, lookup);	
 	cout<<"\nRead SNP lookup table\n";
 	cerr <<" First mrate "<< lookup.mrate(1,1) <<" Last "<< lookup.mrate(100,10)<<endl;
 	cerr <<" First code "<< lookup.code(1,1) <<" Last "<< lookup.code(100,10)<<endl;
@@ -64,21 +68,34 @@ int parse(char* ped_file, char* bcf_file, double snp_mrate, double indel_mrate,
     // Create INDEL lookup
 	vector<vector<string > > tgtIndel;
 	vector<string> tmpIndel;
-    for (int l=0;l<3;l++)
+    for (int l=0; l<3; l++)
         tmpIndel.push_back("NA");
-    for (int l=0;l<9;l++)
+    for (int l=0; l<9; l++)
         tgtIndel.push_back(tmpIndel);
-	lookup_t lookupIndel;
-	makeLookup("indel", snp_mrate, indel_mrate, poly_rate, tgtIndel, lookupIndel);
+	lookup_indel_t lookupIndel;
+	makeIndelLookup(indel_mrate, poly_rate, tgtIndel, lookupIndel);
 	cout<<"\nRead indel lookup table";
 	cerr <<endl<<" First mrate "<< lookupIndel.mrate(1,1) <<" Last "<< lookupIndel.mrate(9,3)<<endl;
     cerr <<" First code "<< lookupIndel.code(1,1) <<" Last "<< lookupIndel.code(9,3)<<endl;
     cerr <<" First tgt "<< tgtIndel[0][0] <<" Last "<< tgtIndel[8][2] <<endl;
     cerr <<" First prior "<< lookupIndel.priors(1,1) <<" Last "<< lookupIndel.priors(9,3) <<endl;
 
+    // Create Pair INDEL lookup
+    vector<vector<string > > tgtPair;
+	vector<string> tmpPair;
+    for (int l=0; l<10; l++)
+        tmpPair.push_back("NA");
+    for (int l=0; l<10; l++)
+        tgtPair.push_back(tmpPair);
+    lookup_pair_t lookupPair;
+    makePairedLookup(pair_mrate, tgtPair, lookupPair);
+    cerr <<" First tgt "<< tgtPair[0][0] <<" Last "<< tgtPair[9][9] <<endl;
+    cerr <<" First prior "<< lookupPair.priors(1,1) <<" Last "<< lookupPair.priors(9,9) <<endl;
+
 	// Iterate each position of BCF file
     qcall_t mom_snp, dad_snp, child_snp;
-	indel_t mom_indel, dad_indel, child_indel;	
+	indel_t mom_indel, dad_indel, child_indel;
+	pair_t tumor, normal;	
 	bcf_hdr_t *hout, *hin;
 	bcf_t *bp = vcf_open(bcf_file, "rb");
 	hin = hout = vcf_hdr_read(bp);
@@ -103,7 +120,20 @@ int parse(char* ped_file, char* bcf_file, double snp_mrate, double indel_mrate,
 				printf("\n Exiting !");
 				exit(1);
 			}
-		}   		  
+		}  
+		
+		for ( j=0; j<pair_count; j++) {
+			int is_indel = bcf2Paired (hout, b, pairs[j], &tumor, &normal, flag);
+			if ( is_indel == 0 ) 
+				pair_like (tumor, normal, tgtPair, lookupPair, flag);	
+			else if ( is_indel < 0 ) {
+				printf("\n BCF PARSING ERROR - Paired Sample!  %d", is_indel);
+				printf("\n Exiting !\n");
+				exit(1);
+			}	
+		}  	
+		
+			  
 	}
 
 	bcf_hdr_destroy(hin);
@@ -117,14 +147,14 @@ int parse(char* ped_file, char* bcf_file, double snp_mrate, double indel_mrate,
 int main(int argc, char *argv[])
 {
 	char ped_f[50] = "EMPTY", bcf_f[50] = "EMPTY";
-	double indel_mrate = 1e-9, snp_mrate = 1e-8, poly_rate = 1e-3, mu_scale = 1.0;
+	double indel_mrate = 1e-9, snp_mrate = 1e-8, poly_rate = 1e-3, mu_scale = 1.0, pair_mrate = 1e-9;
     
     // Read in Command Line arguments
 	while (1) {
 		int option_index = 0;
 		static struct option long_options[] = {{"ped", 1, 0, 0}, 
 			{"bcf", 1, 0, 1}, {"snp_mrate", 1, 0, 2}, {"indel_mrate", 1, 0, 3}, 
-			{"poly_rate", 1, 0, 4}, {"indel_mu_scale", 1, 0, 5}};
+			{"poly_rate", 1, 0, 4}, {"pair_mrates", 1, 0, 5}, {"indel_mu_scale", 1, 0, 6}};
 		int c = getopt_long (argc, argv, "", long_options, &option_index);
 		if (c == -1)
 			break;
@@ -138,25 +168,35 @@ int main(int argc, char *argv[])
 				break;
 			case 2:
 				snp_mrate = atof(optarg);
+				cout<<"\nThe new point mutation rate is : "<<snp_mrate;
 				break;
 			case 3:
 				indel_mrate = atof(optarg);
+				cout<<"\nThe new indel mutation rate is : "<<indel_mrate;
 				break;
 			case 4:
 				poly_rate = atof(optarg);
+				cout<<"\nThe new polymorphism rate is : "<<poly_rate;
 				break;
 			case 5:
+				pair_mrate = atof(optarg);
+				cout<<"\nThe new paired-sample mutation rate is : "<<pair_mrate;
+				break;
+			case 6:
 				mu_scale = atof(optarg);
+				cout<<"\nThe new indel mutation rate scaling factor rate is : "<<mu_scale;
 				break;
 		}
 	}
+  
 	if(!strcmp(bcf_f, "EMPTY") || !strcmp(ped_f, "EMPTY")) {
-		cout<<"ERROR ! Please specify both the PED file and BCF file ! Exiting!\n";
+		cout<<"ERROR ! Please specify both the PED file and BCF file !"
+        "Exiting!\n";
 		exit(1);
 	}
   
     // Create lookup table and read ped, BCF files.
-	parse(ped_f, bcf_f, snp_mrate, indel_mrate, poly_rate, mu_scale);
+	findDenovo(ped_f, bcf_f, snp_mrate, indel_mrate, poly_rate, pair_mrate, mu_scale);
 	cerr<<"\nDone !\n";
 	return 0;
 }
