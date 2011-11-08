f4f4249a8db8b5863f34fd5f9d23b7f3e2ba34a6
Avinash Ramu <trackavinash@gmail.com>
2011-11-08 13:12:49 -0600

    paired sample DNG detection
    Former-commit-id: bb90b9966b1a66d56a421c4004d199c5717c0093 [formerly 0a4a94176f7d305848a4533fa8b7fb6849a40efd]
    Former-commit-id: 8f3b03ddf45ebf92e9d1967898451e3279450573

--- denovogear-code/lookup.h
+++ denovogear-code/lookup.h
@@ -3,14 +3,27 @@
 #include "newmatap.h"
 #include "newmatio.h"
 
+#define MIN_READ_DEPTH_SNP 10
+
+
 using namespace std;
 
-// Struct to hold Lookup Table
+// SNP Lookup Table
 typedef struct {
   Matrix aref; /*priors for "A" reference allele */
   Matrix cref; /*priors for "C" reference allele */
   Matrix gref; /*priors for "G" reference allele */
   Matrix tref; /*priors for "T" reference allele */
+  Matrix snpcode; /*code for identifying a biallelic locus */
+  Matrix tp; /*code for identifying a biallelic locus */
+  Matrix code; /*code for identifying a biallelic locus */
+  Matrix mrate; /*mutation probability*/
+  Matrix denovo; /*code for identifying de novo events + mutation rate*/
+  Matrix norm; /*code for identifying de novo events + mutation rate*/
+} lookup_snp_t;
+
+// Indel Lookup Table
+typedef struct {
   Matrix priors; /*priors for Indel */
   Matrix snpcode; /*code for identifying a biallelic locus */
   Matrix tp; /*code for identifying a biallelic locus */
@@ -19,5 +32,12 @@ typedef struct {
   Matrix denovo; /*code for identifying de novo events + mutation rate*/
   Matrix norm; /*code for identifying de novo events + mutation rate*/
   Matrix hit; /* multiplication factor for indel mu */
-} lookup_t;
+} lookup_indel_t;
 
+// Paired-sample Lookup Table
+typedef struct {
+  Matrix snpcode; /*code for identifying a biallelic locus */
+  Matrix priors; /*priors */
+  Matrix denovo; /*code for identifying de novo events + mutation rate*/
+  Matrix norm; /*code for identifying de novo events + mutation rate*/
+} lookup_pair_t;
--- denovogear-code/parser.h
+++ denovogear-code/parser.h
@@ -10,7 +10,7 @@
 
 #define MAX_QCALL_LINE 2048
 
-// Store QCall Line
+// Store SNP info 
 typedef struct { //New struct for Q calls
 	char chr[3]; /* Chromosome Number. */
 	long pos; /* position, the first base in a chromosome has offset zero. */
@@ -23,7 +23,7 @@ typedef struct { //New struct for Q calls
 	char id[ID_LENGTH]; /* string for sample ID */
 } qcall_t;
 
-// Store INDEL line
+// Store INDEL 
 typedef struct { //New struct for Indels
 	char chr[3]; /* chromosome Number. */
 	long pos; /* position, the first base in a chromosome has offset zero. */
@@ -37,9 +37,24 @@ typedef struct { //New struct for Indels
 	int length; /* to be filled in by Ruth */
 } indel_t;
 
-int parse(char* ped_file, char* bcf_file); // Parse BCF file
+// Store Paired Samples
+typedef struct { //New struct for Q calls
+  char chr[3]; /* Chromosome Number. */
+  long pos; /* position, the first base in a chromosome has offset zero. */
+  char ref_base; /* Either A, C, G or T */
+  char alt[20]; /* ALT string */
+  int depth; /* number of mapped reads */
+  int rms_mapQ; /* RMS mapping quality */
+  int min_lk; /* minimum lk capped at 255 */	
+  int lk[10];   /* log likelihood ratio, capped at 255 */
+  char id[ID_LENGTH]; /* string for sample ID */
+} pair_t;
+
 int bcf_2qcall(bcf_hdr_t *h, bcf1_t *b, Trio t, qcall_t* mom, 
                qcall_t* dad, qcall_t* child, indel_t* mom_indel, 
                indel_t* dad_indel, indel_t* child_indel, int& flag); // BCF to QCALL
+
+int bcf2Paired (bcf_hdr_t *h, bcf1_t *b, Pair p, pair_t* tumor, pair_t* normal, int& flag);
+
 
 #endif
