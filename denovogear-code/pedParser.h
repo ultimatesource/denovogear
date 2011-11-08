74017ff7ea72d3f682d8de5bad59209e7f6bd203
Avinash Ramu <trackavinash@gmail.com>
2011-11-08 13:12:49 -0600

    paired sample DNG detection
    Former-commit-id: f4f4249a8db8b5863f34fd5f9d23b7f3e2ba34a6 [formerly 8f3b03ddf45ebf92e9d1967898451e3279450573] [formerly bb90b9966b1a66d56a421c4004d199c5717c0093 [formerly 0a4a94176f7d305848a4533fa8b7fb6849a40efd]]
    Former-commit-id: d89c20a03e5e0e04544ebb693323e58bf2b14ec7 [formerly c52164b152f039f328f6cbeaee67329941729c1b]
    Former-commit-id: 0a5366dfdbcfe67a95929d8a7e2a39d43479ac34

--- denovogear-code/pedParser.h
+++ denovogear-code/pedParser.h
@@ -1,14 +1,23 @@
-#define MAX_TRIOS 1000
+#define MAX_TRIOS 1000 // max number of trios in the bcf filed
+#define MAX_PAIRS 100 // max number of paired samples in the bcf file
 #define ID_LENGTH 100
 #define LINE_LENGTH 1000
 
 // Trio Structure
 typedef struct  {
-	char fID[ID_LENGTH];	// family ID
-	char cID[ID_LENGTH];	// child sampleID
-	char dID[ID_LENGTH];	// dad sampleID
-	char mID[ID_LENGTH];	// mom sampleID
+  char fID[ID_LENGTH];  // family ID
+  char cID[ID_LENGTH];  // child sampleID
+  char dID[ID_LENGTH];  // dad sampleID
+  char mID[ID_LENGTH];  // mom sampleID
 } Trio;
 
+// Pair Structure
+typedef struct  {
+  char pairID[ID_LENGTH]; // case ID
+  char tumorID[ID_LENGTH];  // tumor sample ID
+  char normalID[ID_LENGTH]; // normal sample ID
+} Pair;
+
 // Parse Trios in PED file
-int parse_ped(const char* ped_file, Trio* trios);
+void parse_ped(const char* ped_file, Trio* trios, Pair* pairs, int & trio_count, 
+                int & pair_count);
