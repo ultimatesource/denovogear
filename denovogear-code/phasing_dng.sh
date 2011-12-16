678e4f37b5cd57a9b2b941850dba0776864b9216
Avinash Ramu <trackavinash@gmail.com>
2011-12-16 15:15:06 -0600

    phasing cleanup
    Former-commit-id: a8c9280c00b4d96d88a078477cf08070535e79df

---
+++ denovogear-code/phasing_dng.sh
@@ -0,0 +1,5 @@
+#! /usr/bin/bash
+
+#./denovogear phaser --dnm dnms_tableS4 --pgt ch4_GTs --bam CEUTrio.HiSeq.WGS.b37_decoy.NA12878.clean.dedup.recal.bam_chr4.bam --window 1000 > op
+
+./denovogear phaser --dnm dnms_tableS4 --pgt ch1_GTs --bam CEUTrio.HiSeq.WGS.b37_decoy.NA12878.clean.dedup.recal.bam_chr1.bam --window 1000 > op
