0a0994e3b1de8cacf759480b4ba3779a99e795a4
Avinash Ramu <trackavinash@gmail.com>
2012-03-28 13:17:00 -0500

    moved to cmake
    Former-commit-id: 59fff8ead2c21b8a7db1722cea4fe35038755683 [formerly 1f01410eeb420ae9b03f0e7112a27ffbc9ec9b5f]
    Former-commit-id: 360302467ae9b9fec7b7d4f96eb7098f08200fa0

---
+++ denovogear-code/build/src/CMakeFiles/denovogear.dir/cmake_clean.cmake
@@ -0,0 +1,18 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/denovogear.dir/denovogear.cc.o"
+  "CMakeFiles/denovogear.dir/snpLike.cc.o"
+  "CMakeFiles/denovogear.dir/indelLike.cc.o"
+  "CMakeFiles/denovogear.dir/pairLike.cc.o"
+  "CMakeFiles/denovogear.dir/makeLookup.cc.o"
+  "CMakeFiles/denovogear.dir/phaser.cc.o"
+  "CMakeFiles/denovogear.dir/pedParser.cc.o"
+  "CMakeFiles/denovogear.dir/bcf2Qcall.cc.o"
+  "CMakeFiles/denovogear.dir/bcf2Paired.cc.o"
+  "denovogear.pdb"
+  "denovogear"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/denovogear.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
