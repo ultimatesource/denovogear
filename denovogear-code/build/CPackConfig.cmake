59fff8ead2c21b8a7db1722cea4fe35038755683
Avinash Ramu <trackavinash@gmail.com>
2012-03-28 13:17:00 -0500

    moved to cmake
    Former-commit-id: 1f01410eeb420ae9b03f0e7112a27ffbc9ec9b5f

---
+++ denovogear-code/build/CPackConfig.cmake
@@ -0,0 +1,88 @@
+# This file will be configured to contain variables for CPack. These variables
+# should be set in the CMake list file of the project before CPack module is
+# included. Example variables are:
+#   CPACK_GENERATOR                     - Generator used to create package
+#   CPACK_INSTALL_CMAKE_PROJECTS        - For each project (path, name, component)
+#   CPACK_CMAKE_GENERATOR               - CMake Generator used for the projects
+#   CPACK_INSTALL_COMMANDS              - Extra commands to install components
+#   CPACK_INSTALLED_DIRECTORIES           - Extra directories to install
+#   CPACK_PACKAGE_DESCRIPTION_FILE      - Description file for the package
+#   CPACK_PACKAGE_DESCRIPTION_SUMMARY   - Summary of the package
+#   CPACK_PACKAGE_EXECUTABLES           - List of pairs of executables and labels
+#   CPACK_PACKAGE_FILE_NAME             - Name of the package generated
+#   CPACK_PACKAGE_ICON                  - Icon used for the package
+#   CPACK_PACKAGE_INSTALL_DIRECTORY     - Name of directory for the installer
+#   CPACK_PACKAGE_NAME                  - Package project name
+#   CPACK_PACKAGE_VENDOR                - Package project vendor
+#   CPACK_PACKAGE_VERSION               - Package project version
+#   CPACK_PACKAGE_VERSION_MAJOR         - Package project version (major)
+#   CPACK_PACKAGE_VERSION_MINOR         - Package project version (minor)
+#   CPACK_PACKAGE_VERSION_PATCH         - Package project version (patch)
+
+# There are certain generator specific ones
+
+# NSIS Generator:
+#   CPACK_PACKAGE_INSTALL_REGISTRY_KEY  - Name of the registry key for the installer
+#   CPACK_NSIS_EXTRA_UNINSTALL_COMMANDS - Extra commands used during uninstall
+#   CPACK_NSIS_EXTRA_INSTALL_COMMANDS   - Extra commands used during install
+
+
+SET(CPACK_BINARY_BUNDLE "")
+SET(CPACK_BINARY_CYGWIN "")
+SET(CPACK_BINARY_DEB "")
+SET(CPACK_BINARY_DRAGNDROP "")
+SET(CPACK_BINARY_NSIS "")
+SET(CPACK_BINARY_OSXX11 "")
+SET(CPACK_BINARY_PACKAGEMAKER "")
+SET(CPACK_BINARY_RPM "")
+SET(CPACK_BINARY_STGZ "")
+SET(CPACK_BINARY_TBZ2 "")
+SET(CPACK_BINARY_TGZ "")
+SET(CPACK_BINARY_TZ "")
+SET(CPACK_BINARY_ZIP "")
+SET(CPACK_CMAKE_GENERATOR "Unix Makefiles")
+SET(CPACK_COMPONENTS_ALL "")
+SET(CPACK_COMPONENT_UNSPECIFIED_HIDDEN "TRUE")
+SET(CPACK_COMPONENT_UNSPECIFIED_REQUIRED "TRUE")
+SET(CPACK_CREATE_DESKTOP_LINKS "denovogear")
+SET(CPACK_GENERATOR "PackageMaker")
+SET(CPACK_INSTALL_CMAKE_PROJECTS "/Users/dclab/conradlab/denovogear/denovogear-code/denovogear-code/build;DeNovoGear;ALL;/")
+SET(CPACK_INSTALL_PREFIX "/Applications/")
+SET(CPACK_MODULE_PATH "/Users/dclab/conradlab/denovogear/denovogear-code/denovogear-code/Modules")
+SET(CPACK_NSIS_DISPLAY_NAME "DeNovoGear")
+SET(CPACK_NSIS_INSTALLER_ICON_CODE "")
+SET(CPACK_NSIS_INSTALLER_MUI_ICON_CODE "")
+SET(CPACK_NSIS_INSTALL_ROOT "$PROGRAMFILES")
+SET(CPACK_NSIS_PACKAGE_NAME "DeNovoGear")
+SET(CPACK_OUTPUT_CONFIG_FILE "/Users/dclab/conradlab/denovogear/denovogear-code/denovogear-code/build/CPackConfig.cmake")
+SET(CPACK_PACKAGE_DEFAULT_LOCATION "/Applications/")
+SET(CPACK_PACKAGE_DESCRIPTION_FILE "/Users/dclab/conradlab/denovogear/denovogear-code/denovogear-code/README.txt")
+SET(CPACK_PACKAGE_DESCRIPTION_SUMMARY "Discover de novo mutations from deep-sequencing of related individuals.")
+SET(CPACK_PACKAGE_EXECUTABLES "denovogear;DeNovoGear")
+SET(CPACK_PACKAGE_FILE_NAME "denovogear-0.4-7-g3e734ed-Darwin-i386")
+SET(CPACK_PACKAGE_INSTALL_DIRECTORY "DeNovoGear")
+SET(CPACK_PACKAGE_INSTALL_REGISTRY_KEY "DeNovoGear")
+SET(CPACK_PACKAGE_NAME "DeNovoGear")
+SET(CPACK_PACKAGE_NAME_FILE "denovogear")
+SET(CPACK_PACKAGE_RELOCATABLE "true")
+SET(CPACK_PACKAGE_VENDOR "Conrad and Cartwright Labs")
+SET(CPACK_PACKAGE_VERSION "0.4-7-g3e734ed")
+SET(CPACK_PACKAGE_VERSION_MAJOR "0")
+SET(CPACK_PACKAGE_VERSION_MINOR "4")
+SET(CPACK_PACKAGE_VERSION_PATCH "1")
+SET(CPACK_PACKAGING_INSTALL_PREFIX "/denovogear 0.4-7-g3e734ed")
+SET(CPACK_RESOURCE_FILE_LICENSE "/Users/dclab/conradlab/denovogear/denovogear-code/denovogear-code/COPYING.txt")
+SET(CPACK_RESOURCE_FILE_README "/Applications/CMake 2.8-7.app/Contents/share/cmake-2.8/Templates/CPack.GenericDescription.txt")
+SET(CPACK_RESOURCE_FILE_WELCOME "/Applications/CMake 2.8-7.app/Contents/share/cmake-2.8/Templates/CPack.GenericWelcome.txt")
+SET(CPACK_SET_DESTDIR "OFF")
+SET(CPACK_SOURCE_CYGWIN "")
+SET(CPACK_SOURCE_GENERATOR "TGZ")
+SET(CPACK_SOURCE_IGNORE_FILES "/CVS/;/\\.svn/;\\.swp$;\\.#;/#;.*~$;/\\.git/;/CMakeFiles/;CMakeCache\\.txt$;CPack.*Config\\.cmake$;cmake_install\\.cmake$;install_manifest\\.txt$;_CPACK_PACKAGES;_CPack_Packages;\\.vcproj$;\\.dir$;\\.ncb$;\\.sln$;\\.suo$;Makefile$;\\.ilk$;\\.pdb$;/releng/;\\.a$;denovogear[-]0;denovogear$;denovogear\\.exe")
+SET(CPACK_SOURCE_OUTPUT_CONFIG_FILE "/Users/dclab/conradlab/denovogear/denovogear-code/denovogear-code/build/CPackSourceConfig.cmake")
+SET(CPACK_SOURCE_PACKAGE_FILE_NAME "denovogear-0.4-7-g3e734ed")
+SET(CPACK_SOURCE_TBZ2 "")
+SET(CPACK_SOURCE_TGZ "")
+SET(CPACK_SOURCE_TZ "")
+SET(CPACK_SOURCE_ZIP "")
+SET(CPACK_SYSTEM_NAME "Darwin-i386")
+SET(CPACK_TOPLEVEL_TAG "Darwin-i386")
---
+++ denovogear-code/src/bcf2Qcall.cc
@@ -0,0 +1,272 @@
+#include <string.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <errno.h>
+#include <math.h>
+
+#include "parser.h"
+#include "prob1.h"
+#include "kstring.h"
+#include "time.h"
+#include "kseq.h"
+
+
+#define MIN_READ_DEPTH 10
+#define MIN_READ_DEPTH_INDEL 10
+#define MIN_MAPQ 40 
+
+static int8_t nt4_table[256] = {
+	4, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4, 
+	4, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4, 
+	4, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4,  4, 4 /*'-'*/, 4, 4,
+	4, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4, 
+	4, 0, 4, 1,  4, 4, 4, 2,  4, 4, 4, 4,  4, 4, 4, 4, 
+	4, 4, 4, 4,  3, 4, 4, 4, -1, 4, 4, 4,  4, 4, 4, 4, 
+	4, 0, 4, 1,  4, 4, 4, 2,  4, 4, 4, 4,  4, 4, 4, 4, 
+	4, 4, 4, 4,  3, 4, 4, 4, -1, 4, 4, 4,  4, 4, 4, 4, 
+	4, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4, 
+	4, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4, 
+	4, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4, 
+	4, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4, 
+	4, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4, 
+	4, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4, 
+	4, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4, 
+	4, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4
+};
+
+static int read_I16(bcf1_t *b, int anno[16])
+{
+	char *p;
+	int i;
+	if ((p = strstr(b->info, "I16=")) == 0) return -1;
+	p += 4;
+	for (i = 0; i < 16; ++i) {
+		anno[i] = strtol(p, &p, 10);
+		if (anno[i] == 0 && (errno == EINVAL || errno == ERANGE)) return -2;
+		++p;
+	}
+	return 0;
+}
+
+// Convert BCF to Qcall format - for each line iterate through samples and look for particular trio
+// Parsing code adapted from BCFtools code
+int bcf_2qcall(bcf_hdr_t *h, bcf1_t *b, Trio t, qcall_t* mom_snp, qcall_t* dad_snp, qcall_t* child_snp, indel_t* mom_indel, indel_t* dad_indel, indel_t* child_indel, int& flag)
+{
+	int a[4], k, g[10], l, map[4], k1, l1, j, i, i0, anno[16], dp, mq, d_rest, indel = 0, found_trio = 3;
+	char *s;
+	if (bcf_is_indel(b)) { 
+		//printf("\nINDEL position %d", b->pos+1);
+		indel = 1;
+	}
+	if (b->ref[1] != 0 || b->n_alleles > 4) { // ref is not a single base ***
+		//printf("\nposition %d ref not a single base", b->pos+1);
+		//indel = 1;
+		//return 10;
+	} 
+	for (i = 0; i < b->n_gi; ++i)
+		if (b->gi[i].fmt == bcf_str2int("PL", 2)) break;
+	if (i == b->n_gi) return -6; // no PL ***
+	if (read_I16(b, anno) != 0) return -7; // no I16; FIXME: can be improved ***
+	d_rest = dp = anno[0] + anno[1] + anno[2] + anno[3];
+	//if (dp == 0) return -8; // depth is zero ***
+	mq = (int)(sqrt((double)(anno[9] + anno[11]) / dp) + .499);
+	i0 = i;
+	a[0] = nt4_table[(int)b->ref[0]];
+	if (a[0] > 3) { // ref is not A/C/G/T *** 
+		return 10; 
+	}
+	a[1] = a[2] = a[3] = -2; // -1 has a special meaning
+	if (b->alt[0] == 0) return -11; // no alternate allele ***
+	map[0] = map[1] = map[2] = map[3] = -2;
+	map[a[0]] = 0;
+	for (k = 0, s = b->alt, k1 = -1; k < 3 && *s; ++k, s += 2) {
+		if (s[1] != ',' && s[1] != 0)  { // ALT is not single base *** // LINES WITH BOTH SNP, INDEL ??
+			//printf("\nposition %d alt not a single base", b->pos+1); 
+			//indel =1 ;
+			//return 10; 
+		}
+		a[k+1] = nt4_table[(int)*s];
+		if (a[k+1] >= 0) map[a[k+1]] = k+1;
+		else k1 = k+1;
+		if (s[1] == 0) break;
+	}
+	for (k = 0; k < 4; ++k)
+		if (map[k] < 0) map[k] = k1;
+		
+		
+	for (i = 0; i < h->n_smpl; ++i) {	//Iterate through all samples
+		int d;
+		uint8_t *p = static_cast<uint8_t *>(static_cast<uint8_t *>(b->gi[i0].data) + i * b->gi[i0].len);
+		for (j = 0; j < b->gi[i0].len; ++j)
+			if (p[j]) break;
+		d = (int)((double)d_rest / (h->n_smpl - i) + .499);
+		if (d == 0) d = 1;
+		if (j == b->gi[i0].len) d = 0;
+		d_rest -= d;
+		for (k = j = 0; k < 4; ++k) {
+			for (l = k; l < 4; ++l) {
+				int t, x = map[k], y = map[l];
+				if (x > y) t = x, x = y, y = t; // swap
+				g[j++] = p[y * (y+1) / 2 + x];
+			}
+		}
+				
+		//found Mom
+		if( strcmp( t.mID, h->sns[i] ) == 0 ) {
+			found_trio--;
+			
+			if( indel == 0 ) { // Write to Moms SNP object
+				strcpy( mom_snp->chr, h->ns[b->tid] );
+				mom_snp->pos = b->pos+1;
+				mom_snp->ref_base = *b->ref;
+				strcpy(mom_snp->alt, b->alt);
+				mom_snp->depth = d;
+				mom_snp->rms_mapQ = mq;
+				strcpy( mom_snp->id, h->sns[i] );
+				for (l1 = 0; l1 < b->n_gi; ++l1) { //CHECK IF PER SAMPLE DEPTH AVAILABLE
+					if (b->gi[l1].fmt == bcf_str2int("DP", 2)) {
+						mom_snp->depth = ((uint16_t*)b->gi[l1].data)[i];
+						//printf("\ndepth1 %d depth2: %d depth3: %d length: %d\n",((uint16_t*)b->gi[l1].data)[0], ((uint16_t*)b->gi[l1].data)[1], ((uint16_t*)b->gi[l1].data)[2], b->gi[l1].len);
+					}
+				}
+				for (j = 0; j < 10; ++j)
+					mom_snp->lk[j] = g[j];
+				if (mom_snp->rms_mapQ < MIN_MAPQ || mom_snp->depth < MIN_READ_DEPTH) 
+					flag =1;
+				//printf("\nSNP mom position %d depth %d", b->pos+1, mom_snp->depth); 
+			}
+			else {
+				uint8_t *likl = static_cast<uint8_t *>(static_cast<uint8_t *>(b->gi[i0].data) + i * b->gi[i0].len);
+				strcpy( mom_indel->chr, h->ns[b->tid] );
+				mom_indel->pos = b->pos+1;
+				strcpy(mom_indel->ref_base, b->ref);
+				strcpy(mom_indel->alt, b->alt);
+				mom_indel->depth = d;
+				mom_indel->rms_mapQ = mq;
+				strcpy( mom_indel->id, h->sns[i] );
+				//printf("\nsample: %s, pos %d ",h->sns[i], b->pos+1 );
+				for (l1 = 0; l1 < b->n_gi; ++l1) { //CHECK IF PER SAMPLE DEPTH AVAILABLE
+					if (b->gi[l1].fmt == bcf_str2int("DP", 2)) {
+						mom_indel->depth = ((uint16_t*)b->gi[l1].data)[i];
+						//printf("\ndepth1 %d depth2: %d depth3: %d length: %d\n",((uint16_t*)b->gi[l1].data)[0], ((uint16_t*)b->gi[l1].data)[1], ((uint16_t*)b->gi[l1].data)[2], b->gi[l1].len);
+					}
+				}
+				for (j = 0; j < 3; ++j) // R/R, R/A and A/A
+					mom_indel->lk[j] = likl[j];
+				if (mom_indel->rms_mapQ < MIN_MAPQ || mom_indel->depth < MIN_READ_DEPTH) 
+					flag =1;
+				//printf("\nINDEL mom position: %d id: %s depth: %d lik: %d, %d, %d", b->pos+1,  mom_indel->id, d,  likl[0], likl[1], likl[2]); 
+			}
+		} 
+		
+		//found Dad
+		if( strcmp( t.dID, h->sns[i] ) == 0 ) {
+			found_trio--;
+			
+			if( indel == 0 ) { // Write to Dads SNP object
+				strcpy( dad_snp->chr, h->ns[b->tid] );
+				dad_snp->pos = b->pos+1;
+				dad_snp->ref_base = *b->ref;
+				strcpy(dad_snp->alt, b->alt);
+				dad_snp->depth = d;
+				dad_snp->rms_mapQ = mq;
+				strcpy( dad_snp->id, h->sns[i] );
+				//printf("\nsample: %s, pos %d ",h->sns[i], b->pos+1 );
+				for (l1 = 0; l1 < b->n_gi; ++l1) { //CHECK IF PER SAMPLE DEPTH AVAILABLE
+					if (b->gi[l1].fmt == bcf_str2int("DP", 2)) {
+						dad_snp->depth = ((uint16_t*)b->gi[l1].data)[i];
+						//printf("\ndepth1 %d depth2: %d depth3: %d length: %d\n",((uint16_t*)b->gi[l1].data)[0], ((uint16_t*)b->gi[l1].data)[1], ((uint16_t*)b->gi[l1].data)[2], b->gi[l1].len);
+					}
+				}
+				for (j = 0; j < 10; ++j)
+					dad_snp->lk[j] = g[j];
+				if (dad_snp->rms_mapQ < MIN_MAPQ || dad_snp->depth < MIN_READ_DEPTH) 
+					flag =1;
+				//printf("\nSNP dad position %d depth %d", b->pos+1, dad_snp->depth); 
+			}
+			else {
+				uint8_t *likl = static_cast<uint8_t *>(static_cast<uint8_t *>(b->gi[i0].data) + i * b->gi[i0].len);
+				strcpy( dad_indel->chr, h->ns[b->tid] );
+				dad_indel->pos = b->pos+1;
+				strcpy(dad_indel->ref_base, b->ref);
+				strcpy(dad_indel->alt, b->alt);
+				dad_indel->depth = d;
+				dad_indel->rms_mapQ = mq;
+				strcpy( dad_indel->id, h->sns[i] );
+				//printf("\nsample: %s, pos %d ",h->sns[i], b->pos+1 );
+				for (l1 = 0; l1 < b->n_gi; ++l1) { //CHECK IF PER SAMPLE DEPTH AVAILABLE
+					if (b->gi[l1].fmt == bcf_str2int("DP", 2)) {
+						dad_indel->depth = ((uint16_t*)b->gi[l1].data)[i];
+						//printf("\ndepth1 %d depth2: %d depth3: %d length: %d\n",((uint16_t*)b->gi[l1].data)[0], ((uint16_t*)b->gi[l1].data)[1], ((uint16_t*)b->gi[l1].data)[2], b->gi[l1].len);
+					}
+				}
+				for (j = 0; j < 3; ++j) // R/R, R/A and A/A
+					dad_indel->lk[j] = likl[j];
+				if (dad_indel->rms_mapQ < MIN_MAPQ || dad_indel->depth < MIN_READ_DEPTH) 
+					flag =1;
+				//printf("\nINDEL dad position: %d id: %s depth: %d lik: %d, %d, %d", b->pos+1,  dad_indel->id, d,  likl[0], likl[1], likl[2]); 
+			}
+			
+		}
+		
+		//found Child
+		if (strcmp( t.cID, h->sns[i]) == 0 ) {
+			found_trio--;
+			
+			if( indel == 0 ) { // Write to Childs SNP object
+				strcpy( child_snp->chr, h->ns[b->tid] );
+				child_snp->pos = b->pos+1;
+				child_snp->ref_base = *b->ref;
+				strcpy(child_snp->alt, b->alt);
+				child_snp->depth = d;
+				child_snp->rms_mapQ = mq;
+				strcpy( child_snp->id, h->sns[i] );
+				//printf("\nsample: %s, pos %d ",h->sns[i], b->pos+1 );
+				for (l1 = 0; l1 < b->n_gi; ++l1) { //CHECK IF PER SAMPLE DEPTH AVAILABLE
+					if (b->gi[l1].fmt == bcf_str2int("DP", 2)) {
+						child_snp->depth = ((uint16_t*)b->gi[l1].data)[i];
+						//printf("\ndepth1 %d depth2: %d depth3: %d length: %d\n",((uint16_t*)b->gi[l1].data)[0], ((uint16_t*)b->gi[l1].data)[1], ((uint16_t*)b->gi[l1].data)[2], b->gi[l1].len);
+					}
+				}
+				for (j = 0; j < 10; ++j) 
+					child_snp->lk[j] = g[j];
+				if (child_snp->rms_mapQ < MIN_MAPQ || child_snp->depth < MIN_READ_DEPTH) 
+					flag =1;
+				//printf("\nSNP child position %d depth %d", b->pos+1, child_snp->depth); 
+			}
+			else {
+				uint8_t *likl = static_cast<uint8_t *>(static_cast<uint8_t *>(b->gi[i0].data) + i * b->gi[i0].len);
+				strcpy( child_indel->chr, h->ns[b->tid] );
+				child_indel->pos = b->pos+1;
+				strcpy(child_indel->ref_base, b->ref);
+				strcpy(child_indel->alt, b->alt);
+				child_indel->depth = d;
+				child_indel->rms_mapQ = mq;
+				strcpy( child_indel->id, h->sns[i] );
+				//printf("\nsample: %s, pos %d ",h->sns[i], b->pos+1 );
+				for (l1 = 0; l1 < b->n_gi; ++l1) { //CHECK IF PER SAMPLE DEPTH AVAILABLE
+					if (b->gi[l1].fmt == bcf_str2int("DP", 2)) {
+						child_indel->depth = ((uint16_t*)b->gi[l1].data)[i];
+						//printf("\ndepth1 %d depth2: %d depth3: %d length: %d\n",((uint16_t*)b->gi[l1].data)[0], ((uint16_t*)b->gi[l1].data)[1], ((uint16_t*)b->gi[l1].data)[2], b->gi[l1].len);
+					}
+				}
+				for (j = 0; j < 3; ++j) // R/R, R/A and A/A
+					child_indel->lk[j] = likl[j];
+				//printf("\n\nINDEL child position: %d id: %s depth: %d lik: %d, %d, %d", b->pos+1, child_indel->id, d,  likl[0], likl[1], likl[2]); 
+				//printf("\nref base:%s alt:%s quality:%d", child_indel->ref_base, child_indel->alt, child_indel->rms_mapQ);
+			}
+		}
+		
+		
+	}
+	
+	//found entire trio, return
+	if ( found_trio == 0 ) { 
+		return indel;
+	} else {
+		printf("\n\nUnable to find trio, exiting Denovogear! ( %d, %d) ", found_trio, i);
+		return -3; //missing member	
+	}
+	
+}
