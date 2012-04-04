3711feb4d2294321f3782e74d541ea6a5c627f4a
Avinash Ramu <trackavinash@gmail.com>
2012-04-04 13:24:32 -0500

    Added XS and XD models
    Former-commit-id: 7b9df3f1fcfb81417104a3cd81c3eb6d7a8d3c22 [formerly 2b131ce8b43f683fa26713ebfc0a19754e6b31b8]
    Former-commit-id: 87a64da3ec121fa53601ba663cf035180182c843

--- denovogear-code/src/makeLookup.h
+++ denovogear-code/src/makeLookup.h
@@ -9,12 +9,36 @@ void printUsage();
 void writeLookup(ofstream& fout, bool snp);
 void getIndelPriors(string g_gts1, int n_uniqa);
 void getSNPPriors(string g_gts1, int n_uniqa);
+
+// Lookup for paired sample analysis
+void makePairedLookup(double pairMrate, vector<vector<string > > & tgt, lookup_pair_t & lookup);
+
+void setIndelLines(vector<vector<string > > & tgt, 
+	float lines[][27]);
+
+void setSNPLines(vector<vector<string > > & tgt,
+	 float lines[][1000]); 
+
+// SNP Lookup for the autosome model 
 void makeSNPLookup(double SNPMrate, double PolyRate, 
 	vector<vector<string > > & tgt, lookup_snp_t & lookup);
+
+// Indel Lookup for the autosome model 
 void makeIndelLookup(double IndelMrate, double PolyRate, 
 	vector<vector<string > > & tgt, lookup_indel_t & lookupIndel);
-void makePairedLookup(double pairMrate, vector<vector<string > > & tgt, lookup_pair_t & lookup);
-void readIndelLookup(ofstream& fout, vector<vector<string > > & tgt, 
-	float lines[][27]);
-void readSNPLookup(ofstream& fout, vector<vector<string > > & tgt,
-	 float lines[][1000]); 
+
+// SNP Lookup for the XS model
+void makeXSSNPLookup(double SNPMrate, double PolyRate, 
+	vector<vector<string > > & tgt, lookup_snp_t & lookup);
+
+// Indel Lookup for the XS model
+void makeXSIndelLookup(double IndelMrate, double PolyRate, 
+	vector<vector<string > > & tgt, lookup_indel_t & lookupIndel);
+
+// SNP Lookup for the XD model
+void makeXDSNPLookup(double SNPMrate, double PolyRate, 
+	vector<vector<string > > & tgt, lookup_snp_t & lookup);
+
+// Indel Lookup for the XD model
+void makeXDIndelLookup(double IndelMrate, double PolyRate, 
+	vector<vector<string > > & tgt, lookup_indel_t & lookupIndel);
