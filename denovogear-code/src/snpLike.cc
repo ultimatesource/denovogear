05a1b4314ce412cc0082c6ae4fff4d2c09ecb210
Avinash Ramu <trackavinash@gmail.com>
2013-01-03 14:50:49 -0600

    added read depth, posterior probability filter; output nsites filtered,interrogated
    Former-commit-id: 868c5d0d524f0db97c18590055ee56703a932999

--- denovogear-code/src/snpLike.cc
+++ denovogear-code/src/snpLike.cc
@@ -8,17 +8,20 @@
 #include "newmatap.h"
 #include "newmatio.h"
 
-#define MIN_READ_DEPTH_SNP 10
-//#define DEBUG_ENABLED  1
-
 using namespace std;
 
 
 // Calculate DNM and Null PP
 void trio_like_snp( qcall_t child, qcall_t mom, qcall_t dad, int flag, 
-                    vector<vector<string > > & tgt, lookup_snp_t & lookup, 
-                    string op_vcf_f, ofstream& fo_vcf)
+  vector<vector<string > > & tgt, lookup_snp_t & lookup, 
+  string op_vcf_f, ofstream& fo_vcf, double pp_cutoff, int RD_cutoff, int& n_site_fail)
 {
+  // Filter low read depths ( < 10 )
+  if (child.depth < RD_cutoff || mom.depth < RD_cutoff || dad.depth < RD_cutoff) {
+    n_site_fail += 1;
+    return;
+  }
+
   Real a[10];   
   Real maxlike_null, maxlike_denovo, pp_null, pp_denovo, denom;   
   Matrix M(1,10);
@@ -33,31 +36,25 @@ void trio_like_snp( qcall_t child, qcall_t mom, qcall_t dad, int flag,
   int i,j,k,l;  
   int coor = child.pos;
   char ref_name[50];
-  strcpy(ref_name, child.chr); // Name of the reference sequence
-  
-  // Filter low read depths ( < 10 )
-  if (child.depth < MIN_READ_DEPTH_SNP ||
-      mom.depth < MIN_READ_DEPTH_SNP || dad.depth < MIN_READ_DEPTH_SNP) {
-    return;
-  }
-  
+  strcpy(ref_name, child.chr); // Name of the reference sequence 
+    
   //Load Likelihood matrices L(D|Gm), L(D|Gd) and L(D|Gc)
   for (j = 0; j != 10; ++j)	{ 
     #ifdef DEBUG_ENABLED
-  	  cout<<"\n"<<" mom lik "<<mom.lk[j]<<" "<<pow(10, -mom.lk[j]/10); 
-      cout<<" dad lik "<<dad.lk[j]<<" "<<pow(10, -dad.lk[j]/10);
-      cout<<" child lik "<<child.lk[j]<<" "<<pow(10, -child.lk[j]/10); 
+    cout<<"\n"<<" mom lik "<<mom.lk[j]<<" "<<pow(10, -mom.lk[j]/10); 
+    cout<<" dad lik "<<dad.lk[j]<<" "<<pow(10, -dad.lk[j]/10);
+    cout<<" child lik "<<child.lk[j]<<" "<<pow(10, -child.lk[j]/10); 
   	#endif    
     a[j]=pow(10,-mom.lk[j]/10.0); 
   }  
   M<<a;  
 
   for (j = 0; j != 10; ++j) 
-  	a[j] = pow(10, -dad.lk[j]/10.0);
+    a[j] = pow(10, -dad.lk[j]/10.0);
   D<<a;
 
   for (j = 0; j != 10; ++j) 
-  	a[j] = pow(10, -child.lk[j]/10.0);
+    a[j] = pow(10, -child.lk[j]/10.0);
   C<<a;
 
   P = KP(M, D); // 10 * 10
@@ -66,46 +63,46 @@ void trio_like_snp( qcall_t child, qcall_t mom, qcall_t dad, int flag,
   // Combine transmission probs L(Gc | Gm, Gf)
   T = SP(F, lookup.tp); //100 * 10
   #ifdef DEBUG_ENABLED  
-    cout<<"\n\nMOM\n\n";
-    cout << setw(10) << setprecision(10) << M;
-    cout<<"\n\nDAD\n\n";
-    cout << setw(10) << setprecision(10) << D;
-    cout<<"\n\nCHILD\n\n";
-    cout << setw(10) << setprecision(10) << C;
-    cout<<"\n\nP = L(Gm, Gd)\n\n";
-    cout << setw(10) << setprecision(10) << P;
-    cout<<"\n\nF = L(Gc, Gm, Gd)\n\n";
-    cout << setw(10) << setprecision(10) << F;
-    cout<<"\n\nT = L(Gc, Gm, Gd) * L(Gc | Gm, Gf)\n\n";
-    cout << setw(10) << setprecision(10) << T;
+  cout<<"\n\nMOM\n\n";
+  cout << setw(10) << setprecision(10) << M;
+  cout<<"\n\nDAD\n\n";
+  cout << setw(10) << setprecision(10) << D;
+  cout<<"\n\nCHILD\n\n";
+  cout << setw(10) << setprecision(10) << C;
+  cout<<"\n\nP = L(Gm, Gd)\n\n";
+  cout << setw(10) << setprecision(10) << P;
+  cout<<"\n\nF = L(Gc, Gm, Gd)\n\n";
+  cout << setw(10) << setprecision(10) << F;
+  cout<<"\n\nT = L(Gc, Gm, Gd) * L(Gc | Gm, Gf)\n\n";
+  cout << setw(10) << setprecision(10) << T;
   #endif
 
   // Combine prior L(Gm, Gf) 
   switch(mom.ref_base) {
     case 'A':
-	  L = SP(T,lookup.aref); break;
+    L = SP(T,lookup.aref); break;
 
     case 'C':
-	  L = SP(T,lookup.cref); break;
+    L = SP(T,lookup.cref); break;
 
     case 'G':
-	  L = SP(T,lookup.gref); break;
+    L = SP(T,lookup.gref); break;
 
     case 'T':
-	  L = SP(T,lookup.tref); break;
+    L = SP(T,lookup.tref); break;
 
     default: L=T; break;
   }
 
   #ifdef DEBUG_ENABLED
-    cout<<"\n\nlookup.mrate\n\n";
-    cout << setw(10) << setprecision(10) << lookup.mrate;
-    cout<<"\n\nlookup.norm\n\n";
-    cout << setw(10) << setprecision(10) << lookup.norm;
-    cout<<"\n\nlookup.denovo\n\n";
-    cout << setw(10) << setprecision(10) << lookup.denovo;
-    cout<<"\n\nL = L(Gc, Gm, Gd) * L(Gc | Gm, Gf) * L(Gm, Gf)\n\n";
-    cout << setw(10) << setprecision(10) << L;
+  cout<<"\n\nlookup.mrate\n\n";
+  cout << setw(10) << setprecision(10) << lookup.mrate;
+  cout<<"\n\nlookup.norm\n\n";
+  cout << setw(10) << setprecision(10) << lookup.norm;
+  cout<<"\n\nlookup.denovo\n\n";
+  cout << setw(10) << setprecision(10) << lookup.denovo;
+  cout<<"\n\nL = L(Gc, Gm, Gd) * L(Gc | Gm, Gf) * L(Gm, Gf)\n\n";
+  cout << setw(10) << setprecision(10) << L;
   #endif
 
   DN=SP(L,lookup.mrate); 
@@ -113,38 +110,36 @@ void trio_like_snp( qcall_t child, qcall_t mom, qcall_t dad, int flag,
   // Find max likelihood of null configuration
   PP=SP(DN,lookup.norm);   //zeroes out configurations with mendelian error
   maxlike_null = PP.maximum2(i,j);   
- 
+
   #ifdef DEBUG_ENABLED
-    cout<<"\n\nDN\n\n";
-    cout << setw(10) << setprecision(20) << DN;
-    cout<<"\n\nPP normal\n\n";
-    cout << setw(10) << setprecision(20) << PP;
+  cout<<"\n\nDN\n\n";
+  cout << setw(10) << setprecision(20) << DN;
+  cout<<"\n\nPP normal\n\n";
+  cout << setw(10) << setprecision(20) << PP;
   #endif
- 
+
   // Find max likelihood of de novo trio configuration
   PP=SP(DN,lookup.denovo);   //zeroes out configurations with mendelian inheritance
   maxlike_denovo=PP.maximum2(k,l);
 
   #ifdef DEBUG_ENABLED
-    cout<<"\n\nPP denovo\n\n";
-    cout << setw(10) << setprecision(20) << PP;
+  cout<<"\n\nPP denovo\n\n";
+  cout << setw(10) << setprecision(20) << PP;
   #endif
 
   denom=DN.sum();
 
   #ifdef DEBUG_ENABLED
-    cout<<"\nmax_normal i "<<i<<" j "<<j<<" GT "<<tgt[i-1][j-1];
-    cout<<"\nmax_denovo k "<<k<<" l "<<l<<" GT "<<tgt[k-1][l-1];;
-    cout<<"\nDenom is "<<denom;    
+  cout<<"\nmax_normal i "<<i<<" j "<<j<<" GT "<<tgt[i-1][j-1];
+  cout<<"\nmax_denovo k "<<k<<" l "<<l<<" GT "<<tgt[k-1][l-1];;
+  cout<<"\nDenom is "<<denom;    
   #endif  
 
   pp_denovo=maxlike_denovo/denom; //denovo posterior probability
   pp_null=1-pp_denovo; //null posterior probability
 
   // Check for PP cutoff 
-  #ifndef DEBUG_ENABLED
-    if ( pp_denovo > 0.0001 ) 
-  #endif
+  if (pp_denovo > pp_cutoff) 
   {
     cout<<"DENOVO-SNP CHILD ID: "<<child.id;
     cout<<" ref_name: "<<ref_name<<" coor: "<<coor<<" ref_base: "<<mom.ref_base<<" ALT: "<<mom.alt;
@@ -155,7 +150,7 @@ void trio_like_snp( qcall_t child, qcall_t mom, qcall_t dad, int flag,
     cout<<" READ_DEPTH child: "<<child.depth<<" dad: "<<dad.depth<<" mom: "<<mom.depth;
     cout<<" MAPPING_QUALITY child: "<<child.rms_mapQ<<" dad: "<<dad.rms_mapQ<<" mom: "<<mom.rms_mapQ;
     cout<<endl;
-   
+
     if(op_vcf_f != "EMPTY") {
       fo_vcf<<ref_name<<"\t";
       fo_vcf<<coor<<"\t";
@@ -172,8 +167,7 @@ void trio_like_snp( qcall_t child, qcall_t mom, qcall_t dad, int flag,
       fo_vcf<<"DNM_CONFIG:PP_DNM:RD:MQ\t";
       fo_vcf<<tgt[k-1][l-1]<<":"<<pp_denovo<<":"<<child.depth<<":"<<child.rms_mapQ; 
       fo_vcf<<"\n";
-
     }
   }
-	  
+
 }
