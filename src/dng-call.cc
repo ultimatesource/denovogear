75d9373b7b1bb825d3bc16b8bd19689ef1f3b788
reedacartwright <cartwright@asu.edu>
2014-07-07 18:04:15 -0700

    partiall work on pedigree peeling

--- src/dng-call.cc
+++ src/dng-call.cc
@@ -24,6 +24,7 @@
 #include <boost/graph/topological_sort.hpp>
 #include <boost/range/algorithm/copy.hpp>
 #include <boost/range/algorithm/reverse.hpp>
+#include <boost/graph/biconnected_components.hpp>
 
 #include <vector>
 #include <boost/range.hpp>
@@ -34,7 +35,9 @@
 enum nodes_e {
 	a1, a2, a3, a4,
 	b1, b2,
-	c1,
+	c1, c2,
+	a5, c3,
+	d1,
 	N
 };
 const char* name[] = {
@@ -53,7 +56,12 @@ Edge used_by[] = {
 	{a4, b2},
 	{b1, b2},
 	{b1, c1},
-	{b2, c1}
+	{b2, c1},
+	{b1, c2},
+	{b2, c2},
+	{a1, a5},
+	{a1, c3},
+	{a5, c3}
 };
 
 struct Family {
@@ -73,18 +81,148 @@ Family ped[] {
 	{ 7, 5, 6 },
 	{ 8, 5, 6 },
 	{ 9, 0, 0 },
-	{ 10, 1, 9 }
+	{ 10, 1, 9 },
+	{ 11, 0, 10 }
 };
+
+struct node {
+	int id;
+	std::vector<int> families;
+};
+
+struct family {
+	std::vector<int> members;	
+};
+
+std::vector<node> node_store;
+std::vector<family> family_store;
+
+int build_graph(Family *p, size_t sz) {
+	std::map<std::pair<int,int>, int> fam_map;
+	// reset node_store
+	node_store.resize(sz);
+	for(size_t k=0;k<sz;++k) {
+		int u = p[k].child;
+		node_store[u].id = u;
+		node_store[u].families.clear();
+	}
+	// build graph of families
+	family_store.clear();
+	for(size_t k=0;k<sz;++k) {
+		if(p[k].dad == 0 || p[k].mom == 0)
+			continue;
+		// check to see if parent pair already exists
+		auto key = std::make_pair(p[k].dad, p[k].mom);
+		auto ret = fam_map.find(key);
+		if(ret != fam_map.end()) {
+			// family exists, so we just add the child and add it to the family
+			family_store[ret->second].members.push_back(p[k].child);
+			node_store[p[k].child].families.push_back(family_store.size()-1);
+		} else {
+			// family doesn't exist, so we construct it
+			family_store.push_back(family());
+			family_store.back().members.assign({p[k].mom,p[k].dad,p[k].child});
+			int fam_id = family_store.size()-1;
+			fam_map.emplace(key, fam_id);
+			// add the family ids to the nodes
+			node_store[p[k].mom].families.push_back(fam_id);
+			node_store[p[k].dad].families.push_back(fam_id);
+			node_store[p[k].child].families.push_back(fam_id);
+		}
+	}
+	return 0;
+}
+
+int construct_peeling_order() {
+	size_t sz = family_store.size();
+	std::vector<int> peel_order;
+	std::vector<int> peeled(sz,0);
+	// find all families that are peelable
+	for(int i=0;i<sz;++i) {
+		auto &fam = family_store[i];
+	}
+	return 0;
+}
+
+namespace boost {
+  enum edge_family_t { edge_family };
+  enum edge_type_t { edge_type };
+  
+  BOOST_INSTALL_PROPERTY(edge, family);
+  BOOST_INSTALL_PROPERTY(edge, type);
+}
 
 
 int main(int argc, char* argv[]) {
-/*
 	using namespace boost;
-	typedef adjacency_list<vecS, vecS, bidirectionalS> Graph;
+	using namespace std;
 
-	Graph g(used_by, used_by + sizeof(used_by) / sizeof(Edge), N);
-	typedef graph_traits<Graph>::vertex_descriptor Vertex;
-	typedef std::list<Vertex> MakeOrder;
+	typedef adjacency_list<vecS, vecS, undirectedS,no_property,
+		property<edge_family_t, std::size_t,property<edge_type_t, std::size_t>>> graph_t;
+	typedef graph_traits < graph_t >::vertex_descriptor vertex_t;
+	std::vector<vertex_t> art_points;
+
+	// construct graph from pedigree information
+	graph_t g(N);
+	for(auto a : ped) {
+		// check to see if mom and dad have been seen before
+		// TODO: check for parent-child inbreeding
+		auto id = edge(a.dad, a.mom, g);
+		if(!id.second) {
+			id = add_edge(a.dad, a.mom, g);
+			put(edge_type, g, id.first, 0);
+		}
+		// add the meiotic edges
+		id = add_edge(a.mom, a.child, g);
+		put(edge_type, g, id.first, 1);
+		id = add_edge(a.dad, a.child, g);
+		put(edge_type, g, id.first, 1);
+	}
+	clear_vertex(0, g);
+		
+	auto component = get(edge_family, g);
+	auto edtype = get(edge_type, g);
+	auto ret = biconnected_components(g, component,std::back_inserter(art_points));
+	
+	typedef std::vector<std::vector<graph_traits<graph_t>::edge_iterator>> 
+		component_groups_t;
+	component_groups_t groups(ret.first);
+  	graph_traits<graph_t>::edge_iterator ei, ei_end;
+  	for(boost::tie(ei, ei_end) = edges(g); ei != ei_end; ++ei) {
+  		groups[component[*ei]].push_back(ei);
+  	}
+  	auto art = art_points.begin();
+  	for(auto &a : groups) {
+  		cout << "[";
+  		for(auto &b : a) {
+  			const char *line = (edtype(*b) == 1) ? " -> " : " -- ";
+  			cout << " " << (char)(source(*b, g) + 'A') << line
+  				 << (char)(target(*b, g) + 'A');
+  		}
+  		cout << " ]";
+  		// check to see if there is only one spousal pair
+  		ei = ei_end;
+  		for(auto &b : a) {
+  			if(edtype(*b) != 0)
+  				continue;
+  			if(ei != ei_end)
+  				return 0;
+  			ei = b;
+  		}
+  		
+  		
+  		if(art != art_points.end()) {
+  			cout << " " << (char)((*art)+'A');
+  			++art;
+  		}
+  		
+  		
+  		cout << "\n";
+  	}
+	
+	return 0;
+	
+/*	typedef std::list<Vertex> MakeOrder;
 	MakeOrder make_order;
 	boost::topological_sort(g, std::front_inserter(make_order));
 
