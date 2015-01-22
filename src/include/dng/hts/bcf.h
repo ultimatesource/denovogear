eba203cd20e0bd5993a5254b6a8b1ad1e4113d90
kaeldai <kdai1@asu.edu>
2015-01-22 13:55:05 -0700

    Moved all of the hts::bcf:File code into the header to make dng/hts/ portable. Modified the header so that it is more generic and not dng specific. And renamed the class and namespace from bcf -> vcf. The dng specific code for writing vcf files are implemented in helper functions in call.cc
    Also modified the "-o" option so that it can take format bcf:filename and vcf:filename.

---
+++ src/include/dng/hts/bcf.h
@@ -0,0 +1,290 @@
+/*
+ * Copyright (c) 2015 Reed A. Cartwright
+ * Authors:  kdai1 <kdai1@asu.edu>
+ *
+ * This file is part of DeNovoGear.
+ *
+ * DeNovoGear is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+#pragma once
+#ifndef CXX_HTS_VCF_H
+#define CXX_HTS_VCF_H
+
+#include "hts.h"
+
+#include <vector>
+#include <string>
+#include <set>
+#include <htslib/vcf.h>
+
+
+#define VCF_VERSION "VCFv4.1"
+
+namespace hts { namespace bcf { 
+
+/**
+ * File - Class for writing VCF/BCF files to the output, either stdout or to a file
+ * To write to stdout set file="-" in constructor. To write as a BCF file set mode="wb" in constructor.
+ * To use:
+ *  1. call addHdrMetadata() and addSample() to add information to the VCF header
+ *  2. call writeHdr() to finish the header
+ *  3. For each site that will be output call addRecord()
+ *  4. The destructor needs to be called to write to the file.
+ */
+class File : hts::File
+{
+ public:
+  
+  /**
+   * Initialize File/output stream for writing VCF/BCF
+   * @file - file name to write to, use "-" for stdout
+   * @mode: "w" = write VCF, "wb" = write BCF
+   * @source: name of application writing the VCF file
+   */
+  File(const char *file, const char *mode, const char *source)
+    : hts::File(file, mode)
+    {
+      hdr = bcf_hdr_init("w"); // VCF header
+      rec = bcf_init1(); // VCF record/body line
+      
+      std::string s = std::string("#source=") + source;
+      bcf_hdr_append(hdr, s.c_str());
+    }
+
+  File(const File&) = delete;
+
+  File& operator=(File& other)
+    {
+      if(this == &other)
+	return *this;
+      
+      hdr = other.hdr;
+      other.hdr = nullptr;
+
+      rec = other.rec;
+      other.rec = nullptr;
+      
+      contig_ids = other.contig_ids;
+      other.contig_ids.clear();
+    }
+      
+  virtual ~File()
+    {
+      if(rec != nullptr)
+	bcf_destroy1(rec);
+
+      if(hdr != nullptr)
+	bcf_hdr_destroy(hdr);
+
+    }
+
+  /** AddHeaderMetadata() - Adds a "##key=value" line to the VCF header */
+  void AddHeaderMetadata(const char *key, const char *value)
+  {
+    if(key == nullptr || value == nullptr)
+      return;
+
+    std::string line = std::string("##") + key + "=" + value;
+    bcf_hdr_append(hdr, line.c_str());
+  }
+
+  void AddHeaderMetadata(const char *key, double value)
+  {
+    AddHeaderMetadata(key, std::to_string(value).c_str());
+  }
+
+  void AddHeaderMetadata(const char *key, int value)
+  {
+    AddHeaderMetadata(key, std::to_string(value).c_str());
+  }
+
+  /** Use this version to add a new INFO/FILTER/FORMAT string to the header */
+  void AddHeaderMetadata(const char *line)
+  {
+    if(line != nullptr)
+      bcf_hdr_append(hdr, line);
+  }
+
+  /** Add another sample/genotype field to the VCF file. */
+  void AddSample(const char *sample)
+  {
+    bcf_hdr_add_sample(hdr, sample);
+  }
+
+  /** Creates the header field. Call only after adding all the sample fields */
+  void WriteHeader()
+  {
+    bcf_hdr_set_version(hdr, "VCF_VERSION");
+   
+    bcf_hdr_add_sample(hdr, NULL); // libhts requires NULL sample before it will write out all the other samples
+    bcf_hdr_write(handle(), hdr);
+  }
+
+
+  /** 
+   * SetID() - Sets the CHROM, POS, and ID fields in the VCF record.
+   * chrom is required field. If id is NULL then ID value will default to "."
+   */
+  void SetID(const char *chrom, int pos, const char *id)
+  {
+    // CHROM
+    if(chrom == NULL)
+      return; // should display some error?
+
+    std::string chrom_s(chrom);
+    if(contig_ids.find(chrom_s) == contig_ids.end())
+      {
+	// there needs to be a matching contig id in the header otherwise bcf_write1() will fault [tracked issue to vcf.cc vcf_format()]
+	std::string conv = std::string("##contig=<ID=") + chrom_s + ",length=1>";
+	bcf_hdr_append(hdr, conv.c_str());
+	contig_ids.insert(chrom_s);
+	//stringstream conv;
+	//conv << "##contig=<ID=" << chrom_s << ",length=1>"; // length is required by htslib but not sure what is the appropiate value?
+	//bcf_hdr_append(hdr, conv.str().c_str());
+	//bcf_hdr_write(fp,hdr); //TODO: unless we call write the ##contig header won't show up in the output
+	//contig_ids.insert(chrom_s);
+      }
+    rec->rid = bcf_hdr_name2id(hdr, chrom);
+
+    // POS
+    rec->pos = pos;
+
+    // ID
+    if(id != NULL)
+      {
+	bcf_update_id(hdr, rec, id);
+      }
+  }
+
+  void SetQuality(int quality)
+  {
+    rec->qual = quality;
+  }
+
+  void setFilter(const char *filter)
+  {
+    if(filter != nullptr)
+      {
+	int32_t fid = bcf_hdr_id2int(hdr, BCF_DT_ID, "PASS");
+	bcf_update_filter(hdr, rec, &fid, 1);
+      }
+  }
+
+  /**
+   * SetAlleles() - Set the REF and ALT fields in the current record
+   * @alleles: A list of all the alleles that show up in the sample. 
+   *          alleles[0] is used as the REF value.
+   */
+  void SetAlleles(std::vector<std::string> &alleles)
+  {
+    if(alleles.size() == 0)
+      return;
+
+    // convert to a comma separated list 
+    std::stringstream ss;
+    for(std::size_t a = 0; a < alleles.size(); a++)
+      ss << "," << alleles[a];
+    
+    bcf_update_alleles_str(hdr, rec, ss.str().c_str());
+  }
+
+
+  /**
+   * UpdateInfoField() - Add another key=Value pair to the INFO field
+   * @key: must be defined in the Header or won't be added.
+   *
+   * TODO: Add a vector<> version of flat, ints, and strings.
+   */
+  void UpdateInfo(const char *key, float value)
+  {
+    bcf_update_info_float(hdr, rec, key, &value, 1);
+  }
+
+  void UpdateInfo(const char *key, int32_t value)
+  {
+    bcf_update_info_int32(hdr, rec, key, &value, 1);
+  }
+
+  void UpdateInfo(const char *key, std::string &value)
+  {
+    if(value.empty())
+      return;
+
+    bcf_update_info_string(hdr, rec, key, value.c_str());
+  }
+
+
+  /**
+   * UpdateSample() - Add a key to the FORMAT field and update each sample column
+   *                  with the corresponding key values.
+   * @name: the tag name that appears in FORMAT field. Must be defined in the header
+   * @data: A list of values that will populate the sample/genotype fields. The vector
+   *        should be a multiple of the number sample columns. 
+   */
+  void UpdateSamples(const char *name, std::vector<float> &data)
+  {
+    float *values = new float[data.size()];
+    for(std::size_t a = 0; a < data.size(); a++)
+      values[a] = data[a];
+    bcf_update_format_float(hdr, rec, name, values, data.size());
+
+    delete [] values;
+  }
+
+  void UpdateSamples(const char *name, std::vector<int32_t> &data)
+  {
+    int32_t *values = new int32_t[data.size()];
+    for(std::size_t a = 0; a < data.size(); a++)
+      values[a] = data[a];
+    bcf_update_format_int32(hdr, rec, name, values, data.size()); 
+
+    delete [] values;
+  }
+
+  
+  void UpdateSamples(const char *name, std::vector<std::string> &data)
+  {
+    const char **values = new const char*[data.size()];
+    for(std::size_t a = 0; a < data.size(); a++)
+      values[a] = data[a].c_str();
+    bcf_update_format_int32(hdr, rec, name, values, data.size());
+
+    delete [] values;
+  }
+
+
+  /** Writes out the up-to-date info in the record and prepares for the next line */
+  void WriteRecord()
+  {
+    // Add line to the body of the VCF
+    bcf_write1(handle(), hdr, rec);
+    
+    // reset the record for the next line
+    bcf_clear(rec);
+  }
+  
+
+  
+						       
+ protected:
+  bcf_hdr_t *hdr;
+  bcf1_t *rec;
+
+  std::set<std::string> contig_ids; // List of unique CHROM/contig values
+
+};
+  
+
+}}
+
+#endif /* CXX_HTS_VCF_H */
--- src/lib/call.cc
+++ src/lib/call.cc
@@ -43,6 +43,8 @@
 
 #include <htslib/faidx.h>
 
+#define SOURCE "Denovogear 1.0.2"
+
 using namespace dng::task;
 using namespace dng;
 namespace x3 = boost::spirit::x3;
@@ -56,6 +58,96 @@ istreambuf_range(std::basic_istream<Elem, Traits>& in)
         std::istreambuf_iterator<Elem, Traits>(in),
         std::istreambuf_iterator<Elem, Traits>());
 }
+
+void vcf_GetMode(std::string &arg, std::string &mode)
+{
+  
+
+}
+
+// Helper function for writing the necessary 
+void vcf_AddHeaderText(hts::bcf::File &vcfout, Call::argument_type &arg)
+{
+  vcfout.AddHeaderMetadata("region", arg.region.c_str());
+  vcfout.AddHeaderMetadata("min_qlen", arg.min_qlen);
+  vcfout.AddHeaderMetadata("min_mapqual", arg.min_mapqual);
+  vcfout.AddHeaderMetadata("mu", arg.mu);	
+  vcfout.AddHeaderMetadata("mu_somatic", arg.mu_somatic);
+  vcfout.AddHeaderMetadata("mu_library", arg.mu_library);
+  vcfout.AddHeaderMetadata("theta", arg.theta);
+  vcfout.AddHeaderMetadata("ref_weight", arg.ref_weight);
+  vcfout.AddHeaderMetadata("min_basequal", arg.min_basequal);
+
+  // Add the available tags for INFO, FILTER, and FORMAT fields
+  // TODO: The commented lines are standard VCF fields that may be worth adding to dng output
+  vcfout.AddHeaderMetadata("##INFO=<ID=LL,Number=1,Type=Float,Description=\"Log likelihood\">");
+  vcfout.AddHeaderMetadata("##INFO=<ID=PMUT,Number=1,Type=Float,Description=\"Probability of mutation\">");
+  //vcfout.AddHeaderField("##INFO=<ID=NS,Number=1,Type=Integer,Description=\"Number of Samples With Data\">");
+  //vcfout.AddHeaderField("##INFO=<ID=DP,Number=1,Type=Integer,Description=\"Total Depth\">");
+  //vcfout.AddHeaderField("##INFO=<ID=AF,Number=A,Type=Float,Description=\"Allele Frequency\">");
+  //vcfout.AddHeaderField("##INFO=<ID=AA,Number=1,Type=String,Description=\"Ancestral Allele\">");
+  vcfout.AddHeaderMetadata("##FILTER=<ID=PASS,Description=\"All filters passed\">");
+  //vcfout.AddHeaderField("##FORMAT=<ID=GT,Number=1,Type=String,Description=\"Genotype\">");
+  //vcfout.AddHeaderField("##FORMAT=<ID=GQ,Number=1,Type=Integer,Description=\"Genotype Quality\">");
+  //vcfout.AddHeaderField("##FORMAT=<ID=DP,Number=1,Type=Integer,Description=\"Read Depth\">");
+  //vcfout.AddHeaderField("##FORMAT=<ID=HQ,Number=2,Type=Integer,Description=\"Haplotype Quality\">");
+  
+  // AD defined http://gatkforums.broadinstitute.org/discussion/1268/how-should-i-interpret-vcf-files-produced-by-the-gatk
+  vcfout.AddHeaderMetadata("##FORMAT=<ID=AD,Number=.,Type=Integer,Description=\"Allelic depths for the ref and alt alleles in the order listed\">");
+
+}
+
+void vcf_AddRecord(hts::bcf::File &vcfout, const char *chrom, int pos, const char ref, double ll, double pmut, std::vector<dng::depth5_t> &read_depths)
+{
+  vcfout.SetID(chrom, pos, nullptr);
+  // vcfout.setQuality() - Need phred scalled measure of the site samples
+  vcfout.setFilter("PASS"); // Currently all sites that make it this far pass the threshold
+  
+  vcfout.UpdateInfo("LL", static_cast<float>(ll));
+  vcfout.UpdateInfo("PMUT", static_cast<float>(pmut));
+
+  // Based on all the samples determine what nucleotides show up and 
+  // the order they will appear in the REF and ALT field
+  std::vector<uint16_t> allele_order; // List of nucleotides as they appear in the REF and ALT fields
+  std::vector<std::string> allele_order_str; // alt_order in string format, used for SetAlleles
+
+  std::size_t ref_index = seq::char_index(ref);
+  allele_order.push_back(ref_index);
+  allele_order_str.push_back(std::string(1, ref));
+  for(std::size_t index = 1; index < 4; index++)
+    {
+      // iterate through the three remaining NTs, if the NT exists in one of the sample add it to alt_order
+      int alt_allele_index = (ref_index + index)%4;
+      for(std::size_t sample = 0; sample < read_depths.size(); sample++)
+	{
+	  if(read_depths[sample].counts[alt_allele_index] != 0)
+	    {
+	      char alt_allele_char = seq::indexed_char(alt_allele_index); 
+	      allele_order.push_back(alt_allele_index);
+	      allele_order_str.push_back(std::string(1, alt_allele_char));
+	    }
+	}
+    }
+
+  // Add REF, ALT fields
+  vcfout.SetAlleles(allele_order_str);
+
+
+  std::vector<int32_t> gtcounts;
+  for(std::size_t sample = 0; sample < read_depths.size(); sample++)
+    {
+      for(std::size_t nt = 0; nt < allele_order.size(); nt++)
+	{
+	  size_t allele_index = allele_order[nt];
+	  gtcounts.push_back(read_depths[sample].counts[allele_index]);
+	}
+    }
+  vcfout.UpdateSamples("AD", gtcounts);
+
+  vcfout.WriteRecord();
+}
+
+
 
 
 // The main loop for dng-call application
@@ -150,28 +242,20 @@ int Call::operator()(Call::argument_type &arg) {
 #else
 
 	// Write VCF header
-	string mode("w");
-	if(boost::algorithm::ends_with(boost::to_upper_copy(arg.output), ".BCF") == true)
-	  {
-	    // if file ends with .bcf write to a bcf file instead of VCF
-	    mode += "b";
-	  }
+	//string mode("w");
+	//if(boost::algorithm::ends_with(boost::to_upper_copy(arg.output), ".BCF") == true)
+	//  {
+	//    // if file ends with .bcf write to a bcf file instead of VCF
+	//    mode += "b";
+	//  }
 	// TODO: Add code to allow for compressed output
 
-	hts::vcf::File vcfout(arg.output.c_str(), mode.c_str(), arg.ped.c_str());
-	//hts::vcf::Vcfostream vcfo("-", "w", "ceu.ped");
-	//vcfo.addHdrMetadata("fasta", arg.fasta.c_str());
-	vcfout.AddHeaderMetadata("region", arg.region.c_str());
-	vcfout.AddHeaderMetadata("min_qlen", arg.min_qlen);
-	vcfout.AddHeaderMetadata("min_mapqual", arg.min_mapqual);
-	vcfout.AddHeaderMetadata("mu", arg.mu);	
-	vcfout.AddHeaderMetadata("mu_somatic", arg.mu_somatic);
-	vcfout.AddHeaderMetadata("mu_library", arg.mu_library);
-	vcfout.AddHeaderMetadata("theta", arg.theta);
-	vcfout.AddHeaderMetadata("ref_weight", arg.ref_weight);
-	vcfout.AddHeaderMetadata("min_basequal", arg.min_basequal);
+	std::string mode; 
+	vcf_GetMode(arg.output, mode);
+	hts::bcf::File vcfout(arg.output.c_str(), mode.c_str(), SOURCE);
+	vcf_AddHeaderText(vcfout, arg);
 	
-	// Add each genotype column
+	// Add each genotype/sample column then save the header
 	for(std::string str : rgs.libraries())
 	  {
 	    std::string sample_name = boost::replace(str, '\t', '.');
@@ -264,7 +348,13 @@ int Call::operator()(Call::argument_type &arg) {
 
 		cout << endl;
 #else
-		vcfout.AddRecord(h->target_name[target_id], position+1, ref_base, d, p, read_depths);
+		vcf_AddRecord(vcfout, h->target_name[target_id], position+1, ref_base, d, p, read_depths);
+		
+		//vcfout.NewRecord(h->target_name[target_id], position+1, ".", ".", "PASS"); 
+		//vcfout.AddInfoField("LL", d);
+		//vcfout.AddInfoField("PMUT", p);
+		//vcfout.writeRecord();
+		//vcfout.AddRecord(h->target_name[target_id], position+1, ref_base, d, p, read_depths);
 #endif
 		return;
 	});
