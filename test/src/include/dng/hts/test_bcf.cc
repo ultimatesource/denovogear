bcd23a18941a5353b5c71575ead5671c323ff0f7
kaeldai <kdai1@asu.edu>
2015-05-20 12:22:05 -0700

    Added unit and package testing into cmake

---
+++ test/src/include/dng/hts/test_bcf.cc
@@ -0,0 +1,11 @@
+#define BOOST_TEST_MODULE "hts::bcf.h"
+
+#include <boost/test/unit_test.hpp>
+#include <boost/test/output_test_stream.hpp>
+#include "dng/hts/bcf.h"
+
+BOOST_AUTO_TEST_CASE(Test_init)
+{
+  //hts::bcf::File vcfout("-", "w", "DNG TEST");
+  //vcfout.WriteHeader();
+}
