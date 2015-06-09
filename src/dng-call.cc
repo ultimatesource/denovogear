055dd0f2ba2ad659e2aaa8fa0deffed4d6d331ce
Reed A. Cartwright <cartwright@asu.edu>
2015-06-08 22:40:52 -0700

    added DEVEL_MODE and timer support to dng-call

--- src/dng-call.cc
+++ src/dng-call.cc
@@ -22,12 +22,19 @@
 #include <dng/app.h>
 #include <dng/task/call.h>
 
+#ifdef DNG_DEVEL
+#   include <boost/timer/timer.hpp>
+#endif
+
+
 // http://www.boost.org/development/requirements.html
 // http://google-styleguide.googlecode.com/svn/trunk/cppguide.xml
 
 typedef dng::CommandLineApp<dng::task::Call> CallApp;
 
 int main(int argc, char *argv[]) {
+    boost::timer::auto_cpu_timer tmr(std::cerr);
+
     try {
         return CallApp(argc, argv)();
     } catch(std::exception &e) {
