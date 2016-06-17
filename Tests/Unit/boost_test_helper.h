e0db5556a79b7d04c2eb47ee7b384523c18ede7c
Steven Wu <stevenhwu@gmail.com>
2016-06-16 17:24:30 -0700

    Add unit test pedigree.cc and update fixture (#158)
    * Add unit test pedigree.cc and update fixture
    
    - Add `fixture`(s) to unittest
      - Fixture allow multiple tests to have the same setup. By factoring it out, multiple unit test files can share the same fixture. Currently it is used in both peeling.cc and pedigree.cc
      - Remove old fixture form peeling.cc
      - Currently `fixtrue_**.h` are in the same folder. This should be ok if there are not too many fixtures.
    - Pass TESTDATA_DIR from CMake to the source code
    - `boost_test_helper.h` - Need to address issue #157, setup both threshold and absolute test for floating point
    - `pedigree.h` - Add three getter() for unittest.
    
    - Clean up, Remove extra file. `test/src/lib/test2_pedigree_v2.cc` was accidentally added to previou commit.
    
    * Redo the unittest with DNG_UNIT_TEST
    
    - DNG_UNIT_TEST use friend class to gain access to protected.
    - Remove getter() methods in pedigree.cc
    - Instead of using `BOOST_FIXTURE_TEST_SUITE(...)` and `BOOST_AUTO_TEST_CASE(...)`, the update version use
    `BOOST_FIXTURE_TEST_CASE(...)`. This avoid forward declaration in the class file
    - Need to make sure they have the same namespace
    
    * Update pedigree.h
    
    Moved `DNG_UNIT_TEST(test_pedigree_inspect);` to the of class definition.

--- Tests/Unit/boost_test_helper.h
+++ Tests/Unit/boost_test_helper.h
@@ -1,8 +1,21 @@
-//
-// Created by steven on 1/15/16.
-//
-// Replace some with with BOOST later
-//
+/*
+ * Copyright (c) 2016 Steven H. Wu
+ * Authors:  Steven H. Wu <stevenwu@asu.edu>
+ *
+ * This file is part of DeNovoGear.
+ *
+ * DeNovoGear is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #ifndef DENOVOGEAR_BOOST_TEST_HELPER_H
 #define DENOVOGEAR_BOOST_TEST_HELPER_H
@@ -10,12 +23,11 @@
 #include <boost/test/test_tools.hpp>
 #include <boost/test/unit_test.hpp>
 
-//TODO: Where should this file go?
 //FIXME: too many global
 //TODO(Reed): I think we should make this configurable somehow from a CMake run. It is important to test both absolute equality and threshold-based equality.
-
+//TODO(SHW): Issue #157
 const double ABS_TEST_THRESHOLD = 1e-10;
-const double BOOST_CLOSE_THRESHOLD = 1e-6;
+const double BOOST_CLOSE_PERCENTAGE_THRESHOLD = 1e-5;
 
 
 template<typename A>
@@ -24,7 +36,7 @@ void boost_check_array(A &expected, A &result, int expected_size) {
     BOOST_CHECK_EQUAL(expected_size, expected.size());
     BOOST_CHECK_EQUAL(expected.size(), result.size());
     for (int i = 0; i < expected.size(); ++i) {
-        BOOST_CHECK_CLOSE(expected[i], result[i], BOOST_CLOSE_THRESHOLD);
+        BOOST_CHECK_CLOSE(expected[i], result[i], BOOST_CLOSE_PERCENTAGE_THRESHOLD);
     }
 
 }
@@ -40,7 +52,7 @@ void boost_check_matrix(M &expected, M &result) {
 				BOOST_CHECK_EQUAL(0, result(i,j));
 			} else {
 				BOOST_CHECK_CLOSE(expected(i, j), result(i, j),
-						BOOST_CLOSE_THRESHOLD);
+						BOOST_CLOSE_PERCENTAGE_THRESHOLD);
 			}
 		}
 	}
@@ -65,10 +77,21 @@ void boost_check_close_vector(V &expected, V2 &result) {
             BOOST_CHECK_EQUAL(0, result[i]);
         }
         else {
-            BOOST_CHECK_CLOSE(expected[i], result[i], BOOST_CLOSE_THRESHOLD);
+            BOOST_CHECK_CLOSE(expected[i], result[i], BOOST_CLOSE_PERCENTAGE_THRESHOLD);
         }
     }
 }
+
+template<typename V, typename V2>
+void boost_check_equal_vector(V &expected, V2 &result) {
+
+	BOOST_CHECK_EQUAL(expected.size(), result.size());
+	for (int i = 0; i < expected.size(); ++i) {
+		BOOST_CHECK(expected[i] == result[i]);
+	}
+}
+
+
 
 template<typename A, typename B>
 void AssertTrue(A expected, B actual){
