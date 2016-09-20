cdbfac758194be81b00ae4b373a41a5fa56adcac
Anders Pitman <tapitman11@gmail.com>
2016-09-20 15:36:36 -0700

    Add test data file

--- test/test_dng_vcf_parser.js
+++ test/test_dng_vcf_parser.js
@@ -4,6 +4,8 @@ var assert = require('assert');
 var dngVCFParser = require('../src/dng_vcf_parser');
 
 describe('dng vcf parser', function() {
-  var vcfText = fs.readFileSync('dng.vcf', 'utf8');
-  dngVCFParser.parseVCFText(vcfText);
+  it('read file', function() {
+    var vcfText = fs.readFileSync('test/data/dng.vcf', 'utf8');
+    dngVCFParser.parseVCFText(vcfText);
+  })
 });
