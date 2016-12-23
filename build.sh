5a5788fc7fa16b6d8bf8afc849e88b73c8db82aa
Anders Pitman <tapitman11@gmail.com>
2016-12-23 14:47:00 -0700

    Restructure repository
    Moved things around a lot. More structured hierarchy now.

--- build.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-python2.7 build.py --rscript_location /usr/bin/Rscript --ped_file test/data/test.ped --source_dir dist --dng_output_file_path test/data/dng_test_output.vcf --output_file_path mutmap.html
