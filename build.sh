5a04edb9713852eb438fa6ea885bf6ade5f49f05
Anders Pitman <tapitman11@gmail.com>
2016-12-23 12:26:44 -0700

    Improve development pipeline
    Deployment and development pipelines now more closely match. Same
    scripts are shared between the two.
    
    Also added argument parsing.

---
+++ build.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python2.7 build.py --rscript_location /usr/bin/Rscript --ped_file test/data/test.ped --source_dir dist --dng_output_file_path test/data/dng_test_output.vcf --output_file_path mutmap.html
