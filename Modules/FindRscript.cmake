0562cf24f2a8548d583d6ec46e667aa32b2a0e66
Anders Pitman <tapitman11@gmail.com>
2016-12-22 20:35:27 -0700

    Improve Rscript cmake discovery
    Use find_package_handle_standard_args to get standard messages.

--- Modules/FindRscript.cmake
+++ Modules/FindRscript.cmake
@@ -1,5 +1,3 @@
 find_program(RSCRIPT_COMMAND Rscript DOC "Rscript executable.")
 
-if (NOT RSCRIPT_COMMAND)
-    message(STATUS "Rscript not found. It is required if you want to generate a visualization via dng mutmap. Please install R with Rscript")
-endif()
+find_package_handle_standard_args(Rscript DEFAULT_MSG RSCRIPT_COMMAND)
