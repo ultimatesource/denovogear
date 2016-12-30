9627c8b5f83736602ec5eb17b5e716fe2d616edb
Anders Pitman <tapitman11@gmail.com>
2016-12-30 16:39:32 -0700

    Add simple server for autotesting
    Since running the entire build.R script is pretty slow due to
    loading the R dependencies, I added a function to the script that
    runs a socket server. When an incoming connection is established,
    it runs the test code. This provides a much faster turnaround
    since the libraries only have to be loaded once when the script
    starts.

--- src/mutmap/tools/build.R
+++ src/mutmap/tools/build.R
@@ -83,13 +83,34 @@ build <- function(sourceDirectory, pedFilePath, dngOutputFilePath,
     write(outputText, outputFilePath)
 }
 
+# designed to be run from within the build/src/mutmap directory
 test <- function() {
-    sourceDirectory <- file.path('dist')
-    pedFilePath <- file.path('test', 'data', 'test.ped')
-    dngOutputFilePath <- file.path('test', 'data', 'dng_test_output.vcf')
+    sourceDirectory <- file.path('.')
+    pedFilePath <- file.path('..', '..', 'testdata', 'relationship_graph',
+                             'relationship_graph.ped')
+    dngOutputFilePath <- file.path('..', '..', 'dng_test_output.vcf')
     outputFilePath <- file.path('mutmap.html')
 
     build(sourceDirectory, pedFilePath, dngOutputFilePath, outputFilePath)
+}
+
+testServer <- function() {
+
+    # use a socket as a simple IPC signalling mechanism. When a connection is
+    # initiated run the test function
+    while(TRUE){
+        con <- socketConnection(host="localhost", port = 6011, blocking=TRUE,
+                                server=TRUE, open="r+")
+        data <- readLines(con, 1)
+        print("Signal received")
+
+        test()
+
+        response <- "Test build completed successfully"
+        writeLines(response, con) 
+
+        close(con)
+    }
 }
 
 main <- function() {
