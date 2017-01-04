b6e9b80b0bc2f7b17a8b445975893964c5ccacfc
Anders Pitman <tapitman11@gmail.com>
2017-01-04 14:21:28 -0700

    Improve argument parsing for mutmap

--- src/mutmap/tools/build.R
+++ src/mutmap/tools/build.R
@@ -1,18 +1,33 @@
 #!@RSCRIPT_COMMAND@ --vanilla
 
+argErrorAndExit <- function(errorMessage) {
+    message <- c(errorMessage, ". Use --help to get usage information")
+    stop(message)
+}
+
 parseArguments <- function() {
 
     library(argparser, quietly=TRUE)
 
     argParser <- arg_parser("Visualization for DeNovoGear")
-    argParser <- add_argument(argParser, "pedFilePath",
+    argParser <- add_argument(argParser, "--pedFilePath", short='-p',
                               help="Path to pedigree file", type="character")
-    argParser <- add_argument(argParser, "dngOutputFilePath",
+    argParser <- add_argument(argParser, "--dngOutputFilePath", short='-d',
                               help="DeNovoGear output VCF", type="character")
-    argParser <- add_argument(argParser, "outputFilePath",
+    argParser <- add_argument(argParser, "--outputFilePath", short='-o',
                               help="Where to write the output",
                               type="character")
     argv <- parse_args(argParser)
+
+    if (is.na(argv$pedFilePath)) {
+        argErrorAndExit("Pedigree file path not provided")
+    }
+    else if (is.na(argv$dngOutputFilePath)) {
+        argErrorAndExit("DeNovoGear output VCF file path not provided")
+    }
+    else if (is.na(argv$outputFilePath)) {
+        argErrorAndExit("Output file path not provided")
+    }
     return(argv)
 }
 
