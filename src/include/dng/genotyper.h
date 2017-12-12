609b7e5ad332ae38cfb8b7f891bb9d4586a6e79f
Reed A. Cartwright <cartwright@asu.edu>
2017-12-11 23:15:47 -0700

    New genotyper

--- src/include/dng/genotyper.h
+++ src/include/dng/genotyper.h
@@ -36,16 +36,6 @@ namespace dng {
 
 namespace genotype {
 
-/* K-Alleles Genotype Model
-
-log P(depths = {a,b,c,...} | genotype = g) =
-    f(1,a,g) + f(2,b,g) + f(3,c,g) + ... + f(T, a+b+..., g)
-
-Case: g = i/i:
-    f(i,a,g) = lgamma(A+n)-lgamma(A) where 
-
-*/
-
 namespace detail {
 class log_pochhammer {
     typedef boost::math::lanczos::lanczos13m53 Lanczos;
@@ -57,10 +47,9 @@ public:
         assert(a > 0.0);
         a_ = a;
         ah_ = a - 0.5;
-        loga_ = log(a);
         agh_ = a + Lanczos::g() - 0.5;
         la_ = log(Lanczos::lanczos_sum(a));
-        logam1_ = loga_-1.0;
+        logam1_ = log(a)-1.0;
     }
 
     double operator()(int n) const {
@@ -68,7 +57,7 @@ public:
     }
 private:
     // store these values for later usage
-    double a_, loga_, agh_, ah_, la_, logam1_;
+    double a_, agh_, ah_, la_, logam1_;
 
     double lnpoch_pos(double n) const {
         assert(n >= 0.0);
@@ -84,126 +73,56 @@ private:
     }
 };
 
-std::array<double,5>
-make_alphas(int reference, int genotype, double phi, double epsilon, double omega);
-}
-
-constexpr int folded_diploid_nucleotides[10][2] = {
-    {0, 0},
-    {0, 1}, {1, 1},
-    {0, 2}, {1, 2}, {2, 2},
-    {0, 3}, {1, 3}, {2, 3}, {3, 3}
-};
-
-// converts from unfolded genotype [0-15] to a folded genotype [0-10]
-constexpr int folded_diploid_genotypes_matrix[4][4] = {
-    {0, 1, 3, 6},
-    {1, 2, 4, 7},
-    {3, 4, 5, 8},
-    {6, 7, 8, 9}
-};
-
-constexpr int folded_diploid_genotypes[16] = {0, 1, 3, 6, 1, 2, 4, 7, 3, 4, 5, 8, 6, 7, 8, 9};
-
-// converts from a folded genotype to an unfolded genotype
-constexpr int unfolded_diploid_genotypes_upper[10] = {0, 1, 5, 2, 6,10, 3, 7,11,15};
-constexpr int unfolded_diploid_genotypes_lower[10] = {0, 4, 5, 8, 9,10,12,13,14,15};
+std::array<double,8> make_alphas(double over_dispersion_hom, 
+        double over_dispersion_het, double error_rate, double ref_bias);
+} // namespace detail
 
 class DirichletMultinomial {
 public:
-    struct params_t {
-        double over_dispersion;     // overdispersion parameter
-        double error_rate; // prob of error when homozygote is sequenced
-        double ref_bias;   // bias towards reference when heterozygote is sequenced
+    using allele_depths_t = dng::pileup::allele_depths_t;
+
+    DirichletMultinomial(double over_dispersion_hom, 
+        double over_dispersion_het, double error_rate, double ref_bias);
+
+    std::pair<GenotypeArray, double> operator()(
+        allele_depths_t::const_reference ad, int num_alleles, int ploidy=2) const;
+
+    double over_dispersion_hom() const { return over_dispersion_hom_; }
+    double over_dispersion_het() const { return over_dispersion_het_; }
+    double error_rate() const { return error_rate_; }
+    double ref_bias() const { return ref_bias_; }
+
+protected:
+    GenotypeArray LogHaploidGenotypes(allele_depths_t::const_reference ad, int num_alleles) const;
+    GenotypeArray LogDiploidGenotypes(allele_depths_t::const_reference ad, int num_alleles) const;
+
+    double over_dispersion_hom_; // overdispersion of homozygotes
+    double over_dispersion_het_; // overdispersion of heterozygotes
+    double error_rate_;      // prob of error when homozygote is sequenced
+    double ref_bias_;        // bias towards reference when heterozygote is sequenced
+
+    using cache_t = std::vector<std::array<double,8>>;
+    using pochhammers_t = std::array<detail::log_pochhammer,8>; 
+    static constexpr int CACHE_SIZE = 512;
+    cache_t cache_{CACHE_SIZE};
+    pochhammers_t pochhammers_;
+
+    enum struct alpha {
+        HOM_MATCH = 0, HOM_ERROR,
+        HET_REF, HET_ALT, HET_ERROR,
+        HET_ALTALT, HOM_TOTAL, HET_TOTAL
     };
-    explicit DirichletMultinomial(params_t params);
 
-    DirichletMultinomial(double over_dispersion, double error_rate, double ref_bias) :
-        DirichletMultinomial(params_t{over_dispersion, error_rate, ref_bias}) {
-
+    inline double pochhammer(alpha a, int n) const {
+        assert(n >= 0);
+        assert(0 <= a < 8);
+        int t = static_cast<int>(a);
+        return (n < CACHE_SIZE) ? cache_[t][n] : pochhammers_[t](n);
     }
 
-    std::pair<GenotypeArray, double> operator()(
-        const pileup::AlleleDepths& depths, size_t pos, int ploidy=2) const;
+    friend std::array<double,8> detail::make_alphas(double over_dispersion_hom, 
+        double over_dispersion_het, double error_rate, double ref_bias);
 
-    std::pair<GenotypeArray, double> operator()(
-        const pileup::RawDepths& depths, int ref_allele, size_t pos, int ploidy=2) const;
-
-    const params_t & parameters() const { return params_; }
-
-protected:
-
-    // NOTE: a = reference; b = genotype; c = nucleotide; d = depth
-    // NOTE: cache_[d][a][c][b]  = log_pochamer(alpha1[a][b][c],d)
-    typedef double cache_type;
-    typedef std::vector<std::array<std::array<std::array<cache_type,10>, 5>,5>> cache_t;
-
-    typedef detail::log_pochhammer model_type;
-
-    typedef std::vector<std::array<std::array<model_type,10>, 5>> model_cache_t;
-
-    static constexpr int kCacheSize = 512;
-
-    cache_t cache_{kCacheSize};
-    model_cache_t models_{5};
-
-    params_t params_;
-};
-
-class DirichletMultinomialMixture {
-public:
-    struct params_t {
-        double pi;      // probability of this component
-        double phi;     // overdispersion parameter
-        double epsilon; // prob of error when homozygote is sequenced
-        double omega;   // bias towards reference when heterozygote is sequenced
-        // fraction of reads that are ref is omega/(1+omega)
-        // Construct a params_t from a string of comma separated values
-        params_t(const std::string &str) {
-            auto f = utility::parse_double_list(str, ',', 4);
-            if(!f.second) {
-                throw std::runtime_error("Unable to parse genotype-likelihood parameters. "
-                                         "It must be a comma separated list of floating-point numbers.");
-            }
-            if(f.first.size() != 4) {
-                throw std::runtime_error("Wrong number of values for genotype-likelihood parameters. "
-                                         "Expected 4; found " + std::to_string(f.first.size()) + ".");
-            }
-            pi = f.first[0];
-            phi = f.first[1];
-            epsilon = f.first[2];
-            omega = f.first[3];
-        }
-        params_t() { }
-    };
-
-    DirichletMultinomialMixture(params_t model_a, params_t model_b);
-
-    std::pair<GenotypeArray, double> operator()(
-        const pileup::AlleleDepths& depths, size_t pos, int ploidy=2) const;
-
-    std::pair<GenotypeArray, double> operator()(
-        const pileup::RawDepths& depths, size_t pos, int ref_allele, int ploidy=2) const;
-
-protected:
-
-    // NOTE: a = reference; b = genotype; c = nucleotide; d = depth
-    // NOTE: cache_[d][a][c][0][b]  = log_pochamer(alpha1[a][b][c],d)
-    // NOTE: cache_[d][a][c][1][b]  = log_pochamer(alpha2[a][b][c],d)
-    typedef double cache_type;
-    typedef std::vector<std::array<std::array<std::array<std::array<cache_type,10>, 2>, 5>,5>> cache_t;
-
-    typedef detail::log_pochhammer model_type;
-
-    typedef std::vector<std::array<std::array<std::array<model_type,10>, 2>, 5>> model_cache_t;
-
-
-    static constexpr int kCacheSize = 512;
-
-    cache_t cache_{kCacheSize};
-    model_cache_t models_{5};
-
-    double f1_, f2_; // log(f) and log(1-f)
 };
 
 } // namespace genotype
@@ -213,4 +132,3 @@ using Genotyper = genotype::DirichletMultinomial;
 } // namespace dng
 
 #endif // DNG_LIKELIHOOD_H
-
